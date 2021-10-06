5400c86269df27b1d65148c63fc2ab7937ba1783
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-06 16:35:24 +0700

    Reconstruct App.js for draft LoginScreen

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaProvider>
+        <LoginScreen />
+      </SafeAreaProvider>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
