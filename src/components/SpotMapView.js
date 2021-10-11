0398f36eee24797f3ffe036f378c344f3993046b
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-11 15:40:55 +0700

    Create draft view for fishing spots map view

---
+++ src/components/SpotMapView.js
@@ -0,0 +1,57 @@
+import { Box } from "native-base";
+import React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+
+import SpotMarker from "./SpotMarker";
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    flex: 1,
+  },
+});
+
+export default function MapViewTest() {
+  return (
+    <Box>
+      <MapView
+        initialRegion={{
+          latitude: 21.038793470613445,
+          longitude: 105.83590283070224,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        style={styles.map}
+      >
+        <SpotMarker
+          coordinate={{
+            latitude: 21.038793470613445,
+            longitude: 105.83590283070224,
+          }}
+          fishingSpot={{
+            name: "Hồ câu thuần việt",
+            rate: 4.4,
+            isVerified: false,
+          }}
+        />
+        <SpotMarker
+          coordinate={{
+            latitude: 21.03517331108639,
+            longitude: 105.85090718809838,
+          }}
+          fishingSpot={{ name: "Hồ Ngọc Bích", rate: 3.4, isVerified: true }}
+        />
+        <SpotMarker
+          coordinate={{
+            latitude: 21.021549290938946,
+            longitude: 105.83967943749226,
+          }}
+          fishingSpot={{ name: "Hồ câu oh yeah", rate: 1.5, isVerified: false }}
+        />
+      </MapView>
+    </Box>
+  );
+}
