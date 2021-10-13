f86ad8cca26399ed3bfe29b064d54cf298cd9c1c
dat-gif <70569870+dat-gif@users.noreply.github.com>
2021-10-14 00:20:27 +0700

    ok

--- App.js
+++ App.js
@@ -4,11 +4,7 @@ import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import components from "./src/config/components";
-<<<<<<< HEAD
 import AnglerCatchReportsHistory from "./src/screens/CatchReportsHistoryDetail";
-=======
-import RootStackNavigator from "./src/navigations/RootStackNavigator";
->>>>>>> app-toan
 
 export default function App() {
   const theme = extendTheme({
@@ -18,16 +14,8 @@ export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
       <SafeAreaProvider>
-<<<<<<< HEAD
         {/* <LogoScreen /> */}
         <AnglerCatchReportsHistory />
-=======
-        <SafeAreaView flex={1}>
-          <NavigationContainer>
-            <RootStackNavigator />
-          </NavigationContainer>
-        </SafeAreaView>
->>>>>>> app-toan
       </SafeAreaProvider>
     </NativeBaseProvider>
   );
