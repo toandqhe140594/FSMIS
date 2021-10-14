22b1979f5a2724785044c7f9d479357cfac8262e
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-14 22:27:44 +0700

    Draft ImageButton component

--- src/components/CatchReport/AddImageButton.js
+++ src/components/CatchReport/AddImageButton.js
@@ -1,11 +1,27 @@
+import { Entypo } from "@expo/vector-icons";
+import { Box, Icon } from "native-base";
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 const AddImageButton = () => {
   return (
-    <View>
-      <Text />
-    </View>
+    <TouchableOpacity>
+      <Box color="muted.500" style={styles.container}>
+        <Icon as={<Entypo name="plus" />} size={10} mr={1} color="muted.500" />
+      </Box>
+    </TouchableOpacity>
   );
 };
 
