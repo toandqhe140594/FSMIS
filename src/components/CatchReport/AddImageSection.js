232d1868a97d89cdff26056438ce90659b009ed9
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-14 18:03:39 +0700

    Draft AddImageSection

---
+++ src/components/CatchReport/AddImageSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+import AddImageButton from "./AddImageButton";
+
+const AddImageSection = () => {
+  return (
+    <View>
+      <AddImageButton />
+    </View>
+  );
+};
+
+export default AddImageSection;
