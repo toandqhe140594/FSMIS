01d74f978259ad116bc831a0ad9076b4e26661d8
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-14 18:03:08 +0700

    Modify UI elements

--- src/screens/AnglerCatchReportScreen.js
+++ src/screens/AnglerCatchReportScreen.js
@@ -1,104 +1,104 @@
 // import { yupResolver } from "@hookform/resolvers/yup";
 import {
-  Box,
+  Button,
+  Center,
+  Checkbox,
   Divider,
   HStack,
-  Image,
   Select,
-  Stack,
   Text,
   VStack,
 } from "native-base";
 import React from "react";
 // import { Controller, useForm } from "react-hook-form";
-import { ScrollView, TextInput } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, StyleSheet, TextInput } from "react-native";
 
+import AddImageSection from "../components/CatchReport/AddImageSection";
 import CatchReportCard from "../components/CatchReport/CatchReportCard";
 // import * as yup from "yup";
 
+const styles = StyleSheet.create({
+  sectionWrapper: {
+    width: "90%",
+  },
+  button: {
+    width: "90%",
+  },
+  selectInput: {
+    width: "75%",
+  },
+  textArea: {
+    borderWidth: 1,
+    textAlignVertical: "top",
+    padding: 5,
+  },
+});
+
 const AnglerCatchReportScreen = () => {
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <VStack flex={1} mx={4} my={3} justifyContent="center" space={2}>
-          <HStack space={3} mb={2} alignItem="center">
-            {/* Holds maximum 3 images */}
-            <Image
-              source={{
-                uri: "https://wallpaperaccess.com/full/317501.jpg",
-              }}
-              alt="Alternate Text"
-              size="xl"
+    <ScrollView>
+      <VStack space={3} divider={<Divider />}>
+        <Center>
+          <VStack style={styles.sectionWrapper}>
+            {/* AddImageButton */}
+            <AddImageSection />
+            {/* Textarea input field */}
+            <TextInput
+              multiline
+              numberOfLines={6}
+              maxLength={1000}
+              placeholder="Mô tả ngày câu của bạn"
+              style={styles.textArea}
             />
-            <Image
-              source={{
-                uri: "https://wallpaperaccess.com/full/317501.jpg",
-              }}
-              alt="Alternate Text"
-              size="xl"
-            />
-            <Image
-              source={{
-                uri: "https://wallpaperaccess.com/full/317501.jpg",
-              }}
-              alt="Alternate Text"
-              size="xl"
-            />
-          </HStack>
-          {/* Textarea input field */}
-          <TextInput
-            multiline
-            numberOfLines={6}
-            maxLength={1000}
-            placeholder="Mô tả ngày câu của bạn"
-            style={{
-              borderWidth: 1,
-              textAlignVertical: "top",
-              padding: 5,
-            }}
-          />
-        </VStack>
+          </VStack>
+        </Center>
 
-        <Divider />
-
-        <Box flex={1} my={3} mx={4} w={{ md: "50%" }}>
-          <Box
+        <Center>
+          <HStack
+            style={styles.sectionWrapper}
             alignItems="center"
             justifyContent="space-between"
-            flexDirection="row"
-            // style={{ borderColor: "blue", borderWidth: 1 }}
           >
-            {/* Select lake type */}
-            <Text bold fontSize="md">
-              Vị trí câu
-            </Text>
+            <Text>Vị trí hồ câu</Text>
+            {/* Select lake type dropdown field */}
             <Select
-              w="75%"
-              // style={{ borderColor: "red", borderWidth: 1 }}
+              style={styles.selectInput}
               accessibilityLabel="Chọn hồ câu"
               fontSize="md"
               placeholder="Chọn hồ câu"
             >
               <Select.Item label="Hồ thường" value={1} />
               <Select.Item label="Hồ VIP" value={2} />
-              <Select.Item label="Hồ VIP" value={3} />
             </Select>
-          </Box>
-        </Box>
+          </HStack>
+        </Center>
 
-        <Divider />
-        <Box flex={1} my={3} mx={4} w={{ md: "50%", lg: "30%" }}>
-          <Text bold fontSize="md" mb={3}>
-            Thông tin cá
-          </Text>
-          <VStack space={2}>
-            <CatchReportCard />
-            <CatchReportCard />
+        <Center>
+          <VStack style={styles.sectionWrapper} space={3}>
+            <Text>Thông tin cá</Text>
+            {/* Catch Report card list */}
+            <VStack>
+              <CatchReportCard />
+            </VStack>
+            {/* Add catch report card button */}
+            <Button style={styles.button} alignSelf="center">
+              Thêm loại cá
+            </Button>
           </VStack>
-        </Box>
-      </ScrollView>
-    </SafeAreaView>
+        </Center>
+
+        <Center>
+          <VStack style={styles.sectionWrapper} space={3}>
+            {/* Public checkbox field */}
+            <Checkbox alignItems="flex-start">Công khai thông tin</Checkbox>
+            {/* Submit button */}
+            <Button style={styles.button} alignSelf="center">
+              Gửi và checkout
+            </Button>
+          </VStack>
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 };
 
