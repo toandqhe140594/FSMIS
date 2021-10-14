0c6d132d5e72e74f2d301a5689cb1bd3414791f0
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-14 18:01:29 +0700

    Draft CatchReportCard

--- src/components/CatchReport/CatchReportCard.js
+++ src/components/CatchReport/CatchReportCard.js
@@ -1,82 +1,76 @@
-import { Box, Button, Checkbox, Select, Text } from "native-base";
+import {
+  Box,
+  Button,
+  Checkbox,
+  HStack,
+  Input,
+  Select,
+  Text,
+  VStack,
+} from "native-base";
 import React from "react";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  rowWrapper: {
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputWrapper: {
+    width: "60%",
+  },
+  textWrapper: {
+    width: "40%",
+  },
+});
 
 const CatchReportCard = () => {
   return (
-    <Box px={2} style={{ borderColor: "blue", borderWidth: 1 }}>
-      <Box
-        mt={3}
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection="row"
-      >
-        {/* Select lake type */}
-        <Text bold>Chọn loài cá</Text>
-        <Select
-          w="62%"
-          h={10}
-          // style={{ borderColor: "red", borderWidth: 1 }}
-          accessibilityLabel="Chọn hồ câu"
-          placeholder="Chọn hồ câu"
-        >
-          <Select.Item label="Cá diếc" value={1} />
-          <Select.Item label="Cá chép" value={2} />
-        </Select>
-      </Box>
-      <Box
-        mt={3}
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection="row"
-      >
-        {/* Select lake type */}
-        <Text bold>Số lượng (con)</Text>
-        <Select
-          w="62%"
-          h={10}
-          // style={{ borderColor: "red", borderWidth: 1 }}
-          accessibilityLabel="Chọn hồ câu"
-          placeholder="Chọn hồ câu"
-        >
-          <Select.Item label="Cá diếc" value={1} />
-          <Select.Item label="Cá chép" value={2} />
-        </Select>
-      </Box>
-      <Box
-        mt={3}
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection="row"
-        // style={{ borderColor: "blue", borderWidth: 1 }}
-      >
-        {/* Select lake type */}
-        <Text bold>Tổng cân nặng (kg)</Text>
-        <Select
-          w="62%"
-          h={10}
-          // style={{ borderColor: "red", borderWidth: 1 }}
-          accessibilityLabel="Chọn hồ câu"
-          placeholder="Chọn hồ câu"
-        >
-          <Select.Item label="Cá diếc" value={1} />
-          <Select.Item label="Cá chép" value={2} />
-        </Select>
-      </Box>
-      <Box
-        mt={3}
-        mb={3}
-        alignItems="center"
-        justifyContent="space-between"
-        flexDirection="row"
-        // style={{ borderColor: "blue", borderWidth: 1 }}
-      >
-        {/* Select lake type */}
-        <Checkbox>Giao lại cho chủ hồ</Checkbox>
-        <Button w="30%" variant="subtle" colorScheme="primary">
-          Xoá
-        </Button>
-      </Box>
-    </Box>
+    <VStack space={2} style={styles.cardWrapper}>
+      <HStack style={styles.rowWrapper}>
+        <Box>
+          <Text fontSize="sm">Chọn loài cá</Text>
+        </Box>
+        <Box style={styles.inputWrapper}>
+          <Select
+            h={10}
+            accessibilityLabel="Chọn hồ câu"
+            placeholder="Chọn hồ câu"
+          >
+            <Select.Item label="Cá diếc" value={1} />
+            <Select.Item label="Cá chép" value={2} />
+          </Select>
+        </Box>
+      </HStack>
+      <HStack style={styles.rowWrapper}>
+        <Box style={styles.textWrapper}>
+          <Text>Số lượng (con)</Text>
+        </Box>
+        <Box style={styles.inputWrapper}>
+          <Input placeholder="Nhập số lượng con" />
+        </Box>
+      </HStack>
+      <HStack style={styles.rowWrapper}>
+        <Box style={styles.textWrapper}>
+          <Text>Tổng cân nặng (kg)</Text>
+        </Box>
+        <Box style={styles.inputWrapper}>
+          <Input h={10} placeholder="Nhập tổng cân nặng" />
+        </Box>
+      </HStack>
+      <HStack style={styles.rowWrapper}>
+        <Checkbox size="" style={{ borderColor: "red", borderWidth: 1 }}>
+          Giao lại cho chủ hồ
+        </Checkbox>
+        <Button w="40%">Xoá</Button>
+      </HStack>
+    </VStack>
   );
 };
 
