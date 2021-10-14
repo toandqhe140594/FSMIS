b6d2ef89301ced7cf8f87b744a4ae8b2336f18d6
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-14 18:03:26 +0700

    Draft AddImageButton

---
+++ src/components/CatchReport/AddImageButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AddImageButton = () => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+export default AddImageButton;
