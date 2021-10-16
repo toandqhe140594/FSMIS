7f08d145a9392f95e5bb4a2c4580f469ba6c3371
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-16 14:22:25 +0700

    Add button to PostEditorScreen

--- src/screens/PostEditorScreen.js
+++ src/screens/PostEditorScreen.js
@@ -1,7 +1,12 @@
-import { Center, Input, Select, VStack } from "native-base";
+import { Button, Center, Input, Select, VStack } from "native-base";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 
 import HeaderTab from "../components/HeaderTab";
 import SingleImageSection from "../components/LakeEditProfile/SingleImageSection";
@@ -18,6 +23,12 @@ const styles = StyleSheet.create({
     textAlignVertical: "top",
     padding: 5,
   },
+  test: {
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  upperContainer: { height: "90%" },
+  bottomContainer: { height: "10%" },
 });
 
 const InputComponent = ({ label, placeholder }) => {
@@ -86,63 +97,73 @@ TextAreaComponent.defaultProps = {
 const PostEditorScreen = () => {
   const [showImageSection, setShowImageSection] = useState(false);
   return (
-    <View>
-      <HeaderTab name="Bài đăng" />
-      <VStack space={3}>
-        <Center>
-          <VStack style={styles.sectionWrapper}>
-            <SelectComponent
-              label="Sự kiện"
-              placeholder="Chọn sự kiện"
-              data={["Thông báo", "Bồi cá", "Báo cá"]}
-            />
-          </VStack>
-        </Center>
-
-        <Center>
-          <TextAreaComponent
-            label="Miêu tả"
-            placeholder=""
-            numberOfLines={3}
-            compStyles={styles.sectionWrapper}
-          />
-        </Center>
-
-        <Center>
-          <VStack style={styles.sectionWrapper}>
-            <Text style={{ fontWeight: "bold" }}>Đính kèm</Text>
-            <Select
-              placeholder="Chọn kiểu đính kèm"
-              onValueChange={(itemValue) => {
-                setShowImageSection(itemValue === "image");
-              }}
-            >
-              <Select.Item label="Ảnh" value="image" my={1} />
-              <Select.Item label="Facebook Video" value="video" my={1} />
-            </Select>
-          </VStack>
-        </Center>
-
-        {showImageSection && (
+    <KeyboardAvoidingView>
+      <VStack style={styles.upperContainer}>
+        <HeaderTab name="Bài đăng" />
+        <VStack space={3}>
           <Center>
             <VStack style={styles.sectionWrapper}>
-              <SingleImageSection />
-            </VStack>
-          </Center>
-        )}
-
-        {!showImageSection && (
-          <Center>
-            <VStack style={styles.sectionWrapper}>
-              <InputComponent
-                placeholder="Nhập link vào đây"
-                label="Đính kèm link"
+              <SelectComponent
+                label="Sự kiện"
+                placeholder="Chọn sự kiện"
+                data={["Thông báo", "Bồi cá", "Báo cá"]}
               />
             </VStack>
           </Center>
-        )}
+
+          <Center>
+            <TextAreaComponent
+              label="Miêu tả"
+              placeholder=""
+              numberOfLines={3}
+              compStyles={styles.sectionWrapper}
+            />
+          </Center>
+
+          <Center>
+            <VStack style={styles.sectionWrapper}>
+              <Text style={{ fontWeight: "bold" }}>Đính kèm</Text>
+              <Select
+                placeholder="Chọn kiểu đính kèm"
+                onValueChange={(itemValue) => {
+                  setShowImageSection(itemValue === "image");
+                }}
+              >
+                <Select.Item label="Ảnh" value="image" my={1} />
+                <Select.Item label="Facebook Video" value="video" my={1} />
+              </Select>
+            </VStack>
+          </Center>
+
+          {showImageSection && (
+            <Center>
+              <VStack style={styles.sectionWrapper}>
+                <SingleImageSection />
+              </VStack>
+            </Center>
+          )}
+
+          {!showImageSection && (
+            <Center>
+              <VStack style={styles.sectionWrapper}>
+                <InputComponent
+                  placeholder="Nhập link vào đây"
+                  label="Đính kèm link"
+                />
+              </VStack>
+            </Center>
+          )}
+        </VStack>
       </VStack>
-    </View>
+      <Center style={styles.bottomContainer}>
+        <VStack style={styles.sectionWrapper}>
+          {/* Submit button */}
+          <Button style={styles.button} alignSelf="center">
+            Đăng
+          </Button>
+        </VStack>
+      </Center>
+    </KeyboardAvoidingView>
   );
 };
 
