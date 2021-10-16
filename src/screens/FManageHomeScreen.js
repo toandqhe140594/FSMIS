fb03ade5f19f0cf5fcae549e0ac35f38bad4d4e5
dat-gif <70569870+dat-gif@users.noreply.github.com>
2021-10-16 21:48:11 +0700

    update route cho menuCategoryForOwner

--- src/screens/FManageHomeScreen.js
+++ src/screens/FManageHomeScreen.js
@@ -4,26 +4,92 @@ import React from "react";
 
 import HeaderTab from "../components/HeaderTab";
 import MenuScreen from "../components/MenuScreen";
+import * as ROUTE_NAMES from "../config/routeNames";
 
 const menuCategoryForOwner = [
-  { id: 1, title: "Xem trang điểm câu của bạn" },
-  { id: 2, title: "Chỉnh sửa thông tin điểm câu" },
-  { id: 3, title: "Quản lý hồ câu" },
-  { id: 4, title: "Quản lý nhân viên" },
-  { id: 5, title: "Xác nhận báo cá" },
-  { id: 6, title: `Lịch sử báo cá` },
-  { id: 7, title: `Quét mã QR` },
-  { id: 8, title: `Lịch sử Check-in` },
-  { id: 9, title: `Quản lý bài đăng` },
-  { id: 10, title: `Đóng cửa khu hồ` },
+  {
+    id: 1,
+    title: "Xem trang điểm câu của bạn",
+    route: ROUTE_NAMES.FLOCATION_OVERVIEW,
+  },
+  {
+    id: 2,
+    title: "Chỉnh sửa thông tin điểm câu",
+    route: ROUTE_NAMES.FLOCATION_PROFILE_EDIT,
+  },
+  {
+    id: 3,
+    title: "Quản lý hồ câu",
+    route: ROUTE_NAMES.FLOCATION_MANAGE_LAKE,
+  },
+  {
+    id: 4,
+    title: "Quản lý nhân viên",
+    route: ROUTE_NAMES.FLOCATION_MANAGE_STAFF,
+  },
+  {
+    id: 5,
+    title: "Xác nhận báo cá",
+    route: ROUTE_NAMES.FLOCATION_CATCH_VERIFY,
+  },
+  {
+    id: 6,
+    title: `Lịch sử báo cá`,
+    route: ROUTE_NAMES.FLOCATION_CATCH_HISTORY,
+  },
+  {
+    id: 7,
+    title: `Quét mã QR`,
+    route: ROUTE_NAMES.FLOCATION_QR_SCAN,
+  },
+  {
+    id: 8,
+    title: `Lịch sử Check-in`,
+    route: ROUTE_NAMES.FLOCATION_CHECKIN_HISTORY,
+  },
+  {
+    id: 9,
+    title: `Quản lý bài đăng`,
+    route: ROUTE_NAMES.FLOCATION_MANAGE_POST,
+  },
+  {
+    id: 10,
+    title: `Đóng cửa khu hồ`,
+    route: ROUTE_NAMES.FLOCATION_CLOSE_FISHING_LOCATION,
+  },
 ];
 const menuCategoryForStaff = [
-  { id: 1, title: "Xem trang điểm câu của bạn" },
-  { id: 2, title: "Xác nhận báo cá" },
-  { id: 3, title: `Lịch sử báo cá` },
-  { id: 4, title: `Quét mã QR` },
-  { id: 5, title: `Lịch sử Check-in` },
-  { id: 6, title: `Quản lý bài đăng` },
+  {
+    id: 1,
+    title: "Xem trang điểm câu của bạn",
+    route: ROUTE_NAMES.FLOCATION_OVERVIEW,
+  },
+
+  {
+    id: 2,
+    title: "Xác nhận báo cá",
+    route: ROUTE_NAMES.FLOCATION_CATCH_VERIFY,
+  },
+  {
+    id: 3,
+    title: `Lịch sử báo cá`,
+    route: ROUTE_NAMES.FLOCATION_CATCH_HISTORY,
+  },
+  {
+    id: 4,
+    title: `Quét mã QR`,
+    route: ROUTE_NAMES.FLOCATION_QR_SCAN,
+  },
+  {
+    id: 5,
+    title: `Lịch sử Check-in`,
+    route: ROUTE_NAMES.FLOCATION_CHECKIN_HISTORY,
+  },
+  {
+    id: 6,
+    title: `Quản lý bài đăng`,
+    route: ROUTE_NAMES.FLOCATION_MANAGE_POST,
+  },
 ];
 
 const logOut = [{ id: 1, title: "Đóng cửa hồ" }];
