045316d9daea63ad399f681abbf6f6709369a144
MoeO17 <coimeozin@gmail.com>
2021-10-16 23:08:05 +0700

    add createLake

---
+++ fsmis/src/main/java/fpt/g31/fsmis/repository/LakeRepos.java
@@ -0,0 +1,7 @@
+package fpt.g31.fsmis.repository;
+
+import fpt.g31.fsmis.entity.Lake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LakeRepos extends JpaRepository<Lake, Long> {
+}
