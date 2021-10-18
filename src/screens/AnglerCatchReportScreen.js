917f888a56712c7163dc71c9f52fc6ad650cec25
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-18 11:25:11 +0700

    Implement custom component

--- src/screens/AnglerCatchReportScreen.js
+++ src/screens/AnglerCatchReportScreen.js
@@ -1,22 +1,15 @@
 // import { yupResolver } from "@hookform/resolvers/yup";
-import {
-  Button,
-  Center,
-  Checkbox,
-  Divider,
-  HStack,
-  Select,
-  Text,
-  VStack,
-} from "native-base";
+import { Button, Center, Checkbox, Divider, Text, VStack } from "native-base";
 import React from "react";
 // import { Controller, useForm } from "react-hook-form";
-import { ScrollView, StyleSheet, TextInput } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 import CatchReportCard from "../components/CatchReport/CatchReportCard";
+import InlineSelectComponent from "../components/common/InlineSelectComponent";
 import MultiImageSection from "../components/common/MultiImageSection";
-import HeaderTab from "../components/HeaderTab";
 // import * as yup from "yup";
+import TextAreaComponent from "../components/common/TextAreaComponent";
+import HeaderTab from "../components/HeaderTab";
 
 const styles = StyleSheet.create({
   sectionWrapper: {
@@ -25,88 +18,65 @@ const styles = StyleSheet.create({
   button: {
     width: "90%",
   },
-  selectInput: {
-    width: "100%",
-  },
-  textArea: {
-    borderWidth: 1,
-    textAlignVertical: "top",
-    padding: 5,
-  },
 });
 
 const AnglerCatchReportScreen = () => {
   return (
-    <ScrollView>
+    <>
       <HeaderTab name="Báo cá" />
-      <VStack space={3} divider={<Divider />}>
-        <Center>
-          <VStack space={2} style={styles.sectionWrapper}>
-            {/* Impage picker section */}
-            <MultiImageSection imageLimit={3} />
-            {/* Textarea input field */}
-            <TextInput
-              multiline
-              numberOfLines={6}
-              maxLength={1000}
-              placeholder="Mô tả ngày câu của bạn"
-              style={styles.textArea}
-            />
-          </VStack>
-        </Center>
-
-        <Center>
-          <HStack
-            style={styles.sectionWrapper}
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Text bold fontSize="md">
-              Vị trí hồ câu
-            </Text>
-            {/* Select lake type dropdown field */}
-            <Select
-              w="70%"
-              accessibilityLabel="Chọn hồ câu"
-              fontSize="md"
-              placeholder="Chọn hồ câu"
-            >
-              <Select.Item label="Hồ thường" value={1} />
-              <Select.Item label="Hồ VIP" value={2} />
-            </Select>
-          </HStack>
-        </Center>
-
-        <Center>
-          <VStack style={styles.sectionWrapper} space={3}>
-            <Text bold fontSize="md">
-              Thông tin cá
-            </Text>
-            {/* Catch Report card list */}
-            <VStack>
-              {/* Should wrap inside <FlatList> */}
-              <CatchReportCard />
-              {/* </FlatList> */}
+      <ScrollView>
+        <VStack space={3} divider={<Divider />}>
+          <Center>
+            <VStack space={2} style={styles.sectionWrapper}>
+              {/* Impage picker section */}
+              <MultiImageSection imageLimit={3} />
+              {/* Textarea input field */}
+              <TextAreaComponent
+                placeholder="Mô tả ngày câu của bạn"
+                numberOfLines={6}
+              />
             </VStack>
-            {/* Add catch report card button */}
-            <Button style={styles.button} alignSelf="center">
-              Thêm loại cá
-            </Button>
-          </VStack>
-        </Center>
+          </Center>
 
-        <Center>
-          <VStack style={styles.sectionWrapper} space={3} mb={3}>
-            {/* Public checkbox field */}
-            <Checkbox alignItems="flex-start">Công khai thông tin</Checkbox>
-            {/* Submit button */}
-            <Button style={styles.button} alignSelf="center">
-              Gửi và checkout
-            </Button>
-          </VStack>
-        </Center>
-      </VStack>
-    </ScrollView>
+          <Center>
+            <InlineSelectComponent
+              myStyles={styles.sectionWrapper}
+              isTitle
+              label="Vị trí hồ câu"
+              placeholder="Chọn hồ câu"
+              data={["Hồ thường", "Hồ VIP"]}
+            />
+          </Center>
+
+          <Center>
+            <VStack style={styles.sectionWrapper} space={3}>
+              <Text bold fontSize="md">
+                Thông tin cá
+              </Text>
+              {/* Catch Report card list */}
+              <VStack>
+                <CatchReportCard />
+              </VStack>
+              {/* Add catch report card button */}
+              <Button style={styles.button} alignSelf="center">
+                Thêm loại cá
+              </Button>
+            </VStack>
+          </Center>
+
+          <Center>
+            <VStack style={styles.sectionWrapper} space={3} mb={3}>
+              {/* Public checkbox field */}
+              <Checkbox alignItems="flex-start">Công khai thông tin</Checkbox>
+              {/* Submit button */}
+              <Button style={styles.button} alignSelf="center">
+                Gửi và checkout
+              </Button>
+            </VStack>
+          </Center>
+        </VStack>
+      </ScrollView>
+    </>
   );
 };
 
