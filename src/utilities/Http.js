fa6d28838348cabadd77590bb641c501340e9b0f
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-18 17:34:48 +0700

    Create axios instance

--- src/utilities/Http.js
+++ src/utilities/Http.js
@@ -1,6 +1,20 @@
 import axios from "axios";
 
+// Create axios client, pre-configured with baseURL
 const http = axios.create({
-  //   baseURL: "https://tradao-web.herokuapp.com/api/v1/",
+  baseURL: "http://103.170.123.80/api/",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+// Set JSON Web Token in Client to be included in all calls
+export const setClientToken = (token) => {
+  http.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
+};
+
 export default http;
