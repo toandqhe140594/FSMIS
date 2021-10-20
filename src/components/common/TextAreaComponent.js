89b0ed654329dffd7e920e64099f0f1a008493df
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-20 16:14:45 +0700

    Add Controller to TextInput

--- src/components/common/TextAreaComponent.js
+++ src/components/common/TextAreaComponent.js
@@ -1,6 +1,7 @@
 import { Box, Text } from "native-base";
 import PropTypes from "prop-types";
 import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
 import { StyleSheet, TextInput } from "react-native";
 
 const styles = StyleSheet.create({
@@ -24,7 +25,12 @@ const TextAreaComponent = ({
   numberOfLines,
   isTitle,
   myStyles,
+  controllerName,
 }) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
   return (
     <Box style={[styles.container, myStyles]}>
       {label.length > 0 && (
@@ -32,13 +38,27 @@ const TextAreaComponent = ({
           {label}
         </Text>
       )}
-      <TextInput
-        multiline
-        numberOfLines={numberOfLines}
-        maxLength={MAX_LENGTH}
-        placeholder={placeholder}
-        style={styles.textArea}
+      <Controller
+        control={control}
+        name={controllerName}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            multiline
+            numberOfLines={numberOfLines}
+            maxLength={MAX_LENGTH}
+            placeholder={placeholder}
+            style={styles.textArea}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+          />
+        )}
       />
+      {errors[controllerName]?.message && (
+        <Text color="red.500" fontSize="xs" italic>
+          {errors[controllerName]?.message}
+        </Text>
+      )}
     </Box>
   );
 };
@@ -49,6 +69,7 @@ TextAreaComponent.propTypes = {
   numberOfLines: PropTypes.number.isRequired,
   myStyles: PropTypes.objectOf(PropTypes.string.isRequired),
   isTitle: PropTypes.bool,
+  controllerName: PropTypes.string.isRequired,
 };
 
 TextAreaComponent.defaultProps = {
