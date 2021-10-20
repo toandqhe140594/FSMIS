af93c280ccfabea4e4c7de95c4658495cd106f34
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-20 17:27:03 +0700

    Edit employee detail box for further usage

--- src/components/EmployeeDetailBox.js
+++ src/components/EmployeeDetailBox.js
@@ -13,6 +13,7 @@ const EmployeeDetailBox = ({
   address,
   image,
   isDetailed,
+  status,
 }) => {
   return (
     <Center flex={1} w="80%">
@@ -42,11 +43,21 @@ const EmployeeDetailBox = ({
             Số điện thoại: <Text>{phoneNumber}</Text>
           </Text>
           <Text style={styles.boldText}>
-            Giới tính: <Text>{gender}</Text>
+            Giới tính: <Text>{gender ? "Nam" : "Nữ"}</Text>
           </Text>
           <Text style={styles.boldText}>
             Địa chỉ: <Text>{address}</Text>
           </Text>
+          {status === "active" && (
+            <Text style={styles.boldText}>
+              Trạng thái: <Text style={{ color: "green" }}>Hoạt động</Text>
+            </Text>
+          )}
+          {status === "inactive" && (
+            <Text style={styles.boldText}>
+              Trạng thái: <Text style={{ color: "red" }}>Ngưng hoạt động</Text>
+            </Text>
+          )}
         </VStack>
       )}
     </Center>
@@ -60,6 +71,7 @@ EmployeeDetailBox.propTypes = {
   gender: PropTypes.bool,
   image: PropTypes.string,
   isDetailed: PropTypes.bool,
+  status: PropTypes.oneOf(["active", "inactive"]),
 };
 EmployeeDetailBox.defaultProps = {
   dob: "",
@@ -67,5 +79,6 @@ EmployeeDetailBox.defaultProps = {
   image: "https://picsum.photos/200",
   address: "",
   isDetailed: false,
+  status: "",
 };
 export default EmployeeDetailBox;
