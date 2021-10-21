1027c5dedea3aa5651bf588967d7ffb93f141f2c
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-21 11:44:09 +0700

    Edit border of TextInput

--- src/components/common/TextAreaComponent.js
+++ src/components/common/TextAreaComponent.js
@@ -2,19 +2,23 @@ import { Box, Text } from "native-base";
 import PropTypes from "prop-types";
 import React from "react";
 import { Controller, useFormContext } from "react-hook-form";
-import { StyleSheet, TextInput } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 const styles = StyleSheet.create({
-  container: {},
   textArea: {
     borderWidth: 1,
+    borderColor: "#e5e5e5",
+    borderRadius: 5,
     textAlignVertical: "top",
     padding: 5,
   },
-  title: {
-    fontWeight: "bold",
-    fontSize: 16,
-  },
+  text: { fontSize: 16 },
+  bold: { fontWeight: "bold" },
 });
 
 const MAX_LENGTH = 1000;
@@ -32,28 +36,32 @@ const TextAreaComponent = ({
     formState: { errors },
   } = useFormContext();
   return (
-    <Box style={[styles.container, myStyles]}>
+    <Box style={myStyles}>
       {label.length > 0 && (
-        <Text style={isTitle ? styles.title : null} mb={2}>
+        <Text style={[styles.text, isTitle ? styles.bold : null]} mb={2}>
           {label}
         </Text>
       )}
+
       <Controller
         control={control}
         name={controllerName}
         render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput
-            multiline
-            numberOfLines={numberOfLines}
-            maxLength={MAX_LENGTH}
-            placeholder={placeholder}
-            style={styles.textArea}
-            onChangeText={onChange}
-            onBlur={onBlur}
-            value={value}
-          />
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <TextInput
+              multiline
+              numberOfLines={numberOfLines}
+              maxLength={MAX_LENGTH}
+              placeholder={placeholder}
+              style={styles.textArea}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+            />
+          </TouchableWithoutFeedback>
         )}
       />
+
       {errors[controllerName]?.message && (
         <Text color="red.500" fontSize="xs" italic>
           {errors[controllerName]?.message}
