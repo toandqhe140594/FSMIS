4fcedf044ad55883f73127a0d8b7fa6ca399220c
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-21 08:47:29 +0700

    Add state and and function props

--- src/components/CatchReport/CatchReportCard.js
+++ src/components/CatchReport/CatchReportCard.js
@@ -1,5 +1,6 @@
-import { Button, Checkbox, HStack, VStack } from "native-base";
-import React from "react";
+import { Box, Button, Checkbox, VStack } from "native-base";
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 
 import InlineInputComponent from "../common/InlineInputComponent";
@@ -9,14 +10,31 @@ const styles = StyleSheet.create({
   cardWrapper: {
     paddingHorizontal: 10,
     paddingVertical: 8,
+    marginVertical: 5,
   },
   rowWrapper: {
+    flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center",
   },
 });
 
-const CatchReportCard = () => {
+const CatchReportCard = ({ id, deleteCard, updateCard }) => {
+  const [fishType, setFishType] = useState("");
+  const [catches, setCatches] = useState("");
+  const [totalWeight, setTotalWeight] = useState("");
+  const [isReleased, setReleased] = useState(false);
+  useEffect(() => {
+    updateCard(id, "fishType", fishType);
+  }, [fishType]);
+  useEffect(() => {
+    updateCard(id, "catches", catches);
+  }, [catches]);
+  useEffect(() => {
+    updateCard(id, "totalWeight", totalWeight);
+  }, [totalWeight]);
+  useEffect(() => {
+    updateCard(id, "isReleased", isReleased);
+  }, [isReleased]);
   return (
     <VStack
       shadow={2}
@@ -27,26 +45,45 @@ const CatchReportCard = () => {
       <InlineSelectComponent
         label="Chọn loài cá"
         placeholder="Nhấp để chọn cá"
+        data={["Cá diếc", "Cá chép"]}
         compact
+        value={fishType}
+        handleOnChange={setFishType}
       />
       <InlineInputComponent
         label="Số lượng (con)"
         placeholder="Nhập số lượng con"
         compact
+        value={catches}
+        handleOnChange={setCatches}
       />
       <InlineInputComponent
         label="Tổng cân nặng (kg)"
         placeholder="Nhập tổng cân nặng"
         compact
+        value={totalWeight}
+        handleOnChange={setTotalWeight}
       />
-      <HStack style={styles.rowWrapper}>
-        <Checkbox>
+      <Box style={styles.rowWrapper}>
+        <Checkbox isChecked={isReleased} onChange={setReleased}>
           <Text style={{ marginLeft: 10 }}>Giao lại cho chủ hồ</Text>
         </Checkbox>
-        <Button w="40%">Xoá</Button>
-      </HStack>
+        <Button w="40%" onPress={() => deleteCard(id)}>
+          Xoá
+        </Button>
+      </Box>
     </VStack>
   );
 };
 
+CatchReportCard.propTypes = {
+  id: PropTypes.string,
+  deleteCard: PropTypes.func,
+  updateCard: PropTypes.func,
+};
+CatchReportCard.defaultProps = {
+  id: "",
+  deleteCard: () => {},
+  updateCard: () => {},
+};
 export default CatchReportCard;
