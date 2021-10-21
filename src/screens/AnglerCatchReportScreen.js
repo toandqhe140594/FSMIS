e3c20a383b321f72e175ca9f85c19d38d73f6e6e
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-21 08:47:42 +0700

    Implement form validation

--- src/screens/AnglerCatchReportScreen.js
+++ src/screens/AnglerCatchReportScreen.js
@@ -1,4 +1,6 @@
-// import { yupResolver } from "@hookform/resolvers/yup";
+import "react-native-get-random-values";
+
+import { yupResolver } from "@hookform/resolvers/yup";
 import {
   Box,
   Button,
@@ -9,16 +11,23 @@ import {
   Text,
   VStack,
 } from "native-base";
-import React from "react";
-// import { Controller, useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import { ScrollView, StyleSheet } from "react-native";
+import { v4 as uuidv4 } from "uuid";
+import * as yup from "yup";
 
 import CatchReportCard from "../components/CatchReport/CatchReportCard";
-import InlineSelectComponent from "../components/common/InlineSelectComponent";
 import MultiImageSection from "../components/common/MultiImageSection";
-// import * as yup from "yup";
+import SelectComponent from "../components/common/SelectComponent";
 import TextAreaComponent from "../components/common/TextAreaComponent";
 import HeaderTab from "../components/HeaderTab";
+
+const validationSchema = yup.object().shape({
+  aCaption: yup.string().required("Họ và tên không thể bỏ trống"),
+  aLakeType: yup.number().required("Loại hồ không được để trống"),
+  isPublic: yup.bool(),
+});
 
 const styles = StyleSheet.create({
   sectionWrapper: {
@@ -30,62 +39,130 @@ const styles = StyleSheet.create({
 });
 
 const AnglerCatchReportScreen = () => {
+  const initCatchCard = {
+    id: uuidv4(),
+    fishType: "",
+    catches: "",
+    totalWeight: "",
+    isReleased: false,
+  };
+  const [cardList, setCardList] = useState([initCatchCard]);
+  const methods = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    resolver: yupResolver(validationSchema),
+  });
+  const { control, handleSubmit } = methods;
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(cardList);
+  };
+  const addCard = () => {
+    const newCard = initCatchCard;
+    setCardList((prev) => [...prev, newCard]);
+  };
+  const deleteCard = (id) => {
+    const newCardList = cardList.filter((card) => card.id !== id);
+    setCardList(newCardList);
+  };
+  const updateCard = (id, name, value) => {
+    const newCardList = cardList.map((card) => {
+      if (card.id === id) {
+        return { ...card, [name]: value };
+      }
+      return card;
+    });
+    setCardList(newCardList);
+  };
   return (
     <>
       <HeaderTab name="Báo cá" />
       <ScrollView>
-        <VStack space={3} divider={<Divider />}>
-          <Center>
-            <Stack space={2} style={styles.sectionWrapper}>
-              {/* Impage picker section */}
-              <MultiImageSection imageLimit={3} />
-              {/* Textarea input field */}
-              <TextAreaComponent
-                placeholder="Mô tả ngày câu của bạn"
-                numberOfLines={6}
+        <FormProvider {...methods}>
+          <VStack space={3} divider={<Divider />}>
+            <Center>
+              <Stack space={2} style={styles.sectionWrapper}>
+                {/* Impage picker section */}
+                <MultiImageSection imageLimit={3} />
+                {/* Textarea input field */}
+                <TextAreaComponent
+                  placeholder="Mô tả ngày câu của bạn"
+                  numberOfLines={6}
+                  controllerName="aCaption"
+                />
+              </Stack>
+            </Center>
+
+            <Center>
+              <SelectComponent
+                myStyle={styles.sectionWrapper}
+                isTitle
+                label="Vị trí hồ câu"
+                placeholder="Chọn hồ câu"
+                data={[
+                  { label: "Hồ thường", val: 1 },
+                  { label: "Hồ VIP", val: 2 },
+                ]}
+                controllerName="aLakeType"
               />
-            </Stack>
-          </Center>
+            </Center>
 
-          <Center>
-            <InlineSelectComponent
-              myStyles={styles.sectionWrapper}
-              isTitle
-              label="Vị trí hồ câu"
-              placeholder="Chọn hồ câu"
-              data={["Hồ thường", "Hồ VIP"]}
-            />
-          </Center>
+            <Center>
+              <Stack style={styles.sectionWrapper} space={2}>
+                <Text bold fontSize="md">
+                  Thông tin cá
+                </Text>
+                {/* Catch Report card list */}
+                <VStack mb={1}>
+                  {cardList.map((card) => (
+                    <CatchReportCard
+                      key={card.id}
+                      id={card.id}
+                      deleteCard={deleteCard}
+                      updateCard={updateCard}
+                    />
+                  ))}
+                </VStack>
+                {/* Add catch report card button */}
+                <Button
+                  style={styles.button}
+                  alignSelf="center"
+                  onPress={addCard}
+                >
+                  Thêm loại cá
+                </Button>
+              </Stack>
+            </Center>
 
-          <Center>
-            <Stack style={styles.sectionWrapper} space={2}>
-              <Text bold fontSize="md">
-                Thông tin cá
-              </Text>
-              {/* Catch Report card list */}
-              <VStack mb={1}>
-                <CatchReportCard />
-              </VStack>
-              {/* Add catch report card button */}
-              <Button style={styles.button} alignSelf="center">
-                Thêm loại cá
-              </Button>
-            </Stack>
-          </Center>
-
-          <Center>
-            <Box style={styles.sectionWrapper} mb={5}>
-              {/* Public checkbox field */}
-              <Checkbox mb={1} alignItems="flex-start">
-                Công khai thông tin
-              </Checkbox>
-              {/* Submit button */}
-              <Button style={styles.button} alignSelf="center">
-                Gửi và checkout
-              </Button>
-            </Box>
-          </Center>
-        </VStack>
+            <Center>
+              <Box style={styles.sectionWrapper} mb={5}>
+                {/* Public checkbox field */}
+                <Controller
+                  control={control}
+                  name="isPublic"
+                  render={({ field: { onChange, value } }) => (
+                    <Checkbox
+                      mb={1}
+                      alignItems="flex-start"
+                      isChecked={value}
+                      onChange={onChange}
+                    >
+                      Công khai thông tin
+                    </Checkbox>
+                  )}
+                />
+                {/* Submit button */}
+                <Button
+                  style={styles.button}
+                  alignSelf="center"
+                  onPress={handleSubmit(onSubmit)}
+                >
+                  Gửi và checkout
+                </Button>
+              </Box>
+            </Center>
+          </VStack>
+        </FormProvider>
       </ScrollView>
     </>
   );
--- src/screens/FManageLakeEditProfileScreen.js
+++ src/screens/FManageLakeEditProfileScreen.js
@@ -1,3 +1,7 @@
+// To install uuid, installs react-native-random-value pkg first
+import "react-native-get-random-values";
+
+import { yupResolver } from "@hookform/resolvers/yup";
 import {
   Box,
   Button,
@@ -8,16 +12,28 @@ import {
   Text,
   VStack,
 } from "native-base";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { StyleSheet } from "react-native";
+import { v4 as uuidv4 } from "uuid";
+import * as yup from "yup";
 
-import InlineInputComponent from "../components/common/InlineInputComponent";
 import InputComponent from "../components/common/InputComponent";
+import SingleImageSection from "../components/common/SingleImageSection";
 import TextAreaComponent from "../components/common/TextAreaComponent";
 import HeaderTab from "../components/HeaderTab";
 import AddFishCard from "../components/LakeEditProfile/AddFishCard";
 import CheckboxSelectorComponent from "../components/LakeEditProfile/CheckboxSelectorComponent";
-import SingleImageSection from "../components/LakeEditProfile/SingleImageSection";
+
+const validationSchema = yup.object().shape({
+  lakeName: yup.string().required("Tên hồ không thể bỏ trống"),
+  lakeDescription: yup.string().required("Miêu tả giá vé ở hồ này"),
+  lakeLength: yup.string().required("Chiều dài hồ không được để trống"),
+  lakeWidth: yup.string().required("Chiều rộng hồ không được để trống"),
+  lakeDepth: yup.string().required("Độ sâu của hồ không được để trống"),
+});
+
+const fishingMethodData = ["Câu đài", "Câu đơn", "Câu lục"];
 
 const styles = StyleSheet.create({
   sectionWrapper: {
@@ -29,93 +45,175 @@ const styles = StyleSheet.create({
 });
 
 const LakeEditProfileScreen = () => {
+  const initFishCard = {
+    id: uuidv4(),
+    fish: "",
+    weightDescription: "",
+    amount: "",
+    totalWeight: "",
+  };
+  const [fishingMethods, setFishingMethods] = useState([]);
+  const [selectedString, setSelectedString] = useState("Chọn loại hình câu");
+  const [cardList, setCardList] = useState([initFishCard]);
+  const methods = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    resolver: yupResolver(validationSchema),
+  });
+  const { handleSubmit } = methods;
+  const onSubmit = (data) => {
+    // Test submit
+    console.log(data);
+    console.log(fishingMethods);
+    console.log(cardList);
+  };
+  const handleOnSelect = (values) => {
+    setFishingMethods(values || []);
+  };
+  useEffect(() => {
+    if (fishingMethods.length === 0) setSelectedString("Chọn loại hình câu");
+    else
+      setSelectedString(
+        fishingMethods.reduce(
+          (accumulator, currentValue) => `${accumulator}, ${currentValue}`,
+        ),
+      );
+  }, [fishingMethods]);
+
+  const addCard = () => {
+    const newCard = initFishCard;
+    setCardList((prev) => [...prev, newCard]);
+  };
+  const deleteCard = (id) => {
+    const newCardList = cardList.filter((card) => card.id !== id);
+    setCardList(newCardList);
+  };
+  const updateCard = (id, name, value) => {
+    const newCardList = cardList.map((card) => {
+      if (card.id === id) {
+        return { ...card, [name]: value };
+      }
+      return card;
+    });
+    setCardList(newCardList);
+  };
+
   return (
     <>
       <HeaderTab name="Chỉnh sửa hồ bé" />
       <ScrollView>
-        <VStack space={3} divider={<Divider />}>
-          <Center mt={1}>
-            {/* Image Picker section */}
-            <SingleImageSection style={styles.sectionWrapper} />
-          </Center>
+        <FormProvider {...methods}>
+          <VStack space={3} divider={<Divider />}>
+            <Center mt={1}>
+              {/* Image Picker section */}
+              <SingleImageSection style={styles.sectionWrapper} />
+            </Center>
 
-          <Center>
-            <InputComponent
-              myStyles={styles.sectionWrapper}
-              label="Tên hồ câu"
-              isTitle
-              placeholder="Nhập tên hồ câu"
-            />
-          </Center>
-
-          <Center>
-            <VStack style={styles.sectionWrapper}>
-              <Text fontSize="md" bold mb={2}>
-                Loại hình câu
-              </Text>
-              <CheckboxSelectorComponent />
-            </VStack>
-          </Center>
-
-          <Center>
-            <TextAreaComponent
-              myStyles={styles.sectionWrapper}
-              label="Giá vé"
-              isTitle
-              placeholder="Miêu tả giá vé hồ"
-              numberOfLines={3}
-            />
-          </Center>
-
-          <Center>
-            <VStack space={2} style={styles.sectionWrapper}>
-              <Text fontSize="md" bold>
-                Thông số
-              </Text>
-              <InlineInputComponent
-                label="Chiều dài (m)"
-                placeholder="Nhập chiều dài hồ"
+            <Center>
+              <InputComponent
+                myStyles={styles.sectionWrapper}
+                label="Tên hồ câu"
+                isTitle
+                placeholder="Nhập tên hồ câu"
+                controllerName="lakeName"
               />
-              <InlineInputComponent
-                label="Chiều rộng (m)"
-                placeholder="Nhập chiều rộng của hồ"
-              />
-              <InlineInputComponent
-                label="Độ sâu (m)"
-                placeholder="Nhập độ sâu của hồ"
-              />
-            </VStack>
-          </Center>
+            </Center>
 
-          <Center>
-            <Stack space={2} style={styles.sectionWrapper}>
-              <Text fontSize="md" bold>
-                Các loại cá
-              </Text>
-              <VStack mb={1}>
-                <AddFishCard />
+            <Center>
+              <VStack space={2} style={styles.sectionWrapper}>
+                <Text fontSize="md" bold>
+                  Loại hình câu
+                </Text>
+                {/* Uncontrolled field */}
+                <CheckboxSelectorComponent
+                  placeholder={selectedString}
+                  groupValue={fishingMethods}
+                  handleOnSelect={handleOnSelect}
+                  data={fishingMethodData}
+                />
               </VStack>
-              <Button style={styles.button} alignSelf="center">
-                Thêm loại cá
-              </Button>
-            </Stack>
-          </Center>
-          <Center>
-            <Box style={styles.sectionWrapper} mb={5}>
-              {/* Submit button */}
-              <Button style={styles.button} alignSelf="center" mb={2}>
-                Lưu thông tin hồ câu
-              </Button>
-              <Button
-                style={styles.button}
-                variant="outline"
-                alignSelf="center"
-              >
-                Xoá hồ câu
-              </Button>
-            </Box>
-          </Center>
-        </VStack>
+            </Center>
+
+            <Center>
+              <TextAreaComponent
+                myStyles={styles.sectionWrapper}
+                label="Giá vé"
+                isTitle
+                placeholder="Miêu tả giá vé hồ"
+                numberOfLines={3}
+                controllerName="lakeDescription"
+              />
+            </Center>
+
+            <Center>
+              <VStack space={2} style={styles.sectionWrapper}>
+                <Text fontSize="md" bold>
+                  Thông số
+                </Text>
+                <InputComponent
+                  label="Chiều dài (m)"
+                  placeholder="Nhập chiều dài của hồ"
+                  controllerName="lakeLength"
+                />
+                <InputComponent
+                  label="Chiều rộng (m)"
+                  placeholder="Nhập chiều rộng của hồ"
+                  controllerName="lakeWidth"
+                />
+                <InputComponent
+                  label="Độ sâu (m)"
+                  placeholder="Nhập độ sâu của hồ"
+                  controllerName="lakeDepth"
+                />
+              </VStack>
+            </Center>
+
+            <Center>
+              <Stack space={2} style={styles.sectionWrapper}>
+                <Text fontSize="md" bold>
+                  Các loại cá
+                </Text>
+                <VStack mb={1}>
+                  {cardList.map((card) => (
+                    <AddFishCard
+                      key={card.id}
+                      id={card.id}
+                      deleteCard={deleteCard}
+                      updateCard={updateCard}
+                    />
+                  ))}
+                </VStack>
+                <Button
+                  style={styles.button}
+                  alignSelf="center"
+                  onPress={addCard}
+                >
+                  Thêm loại cá
+                </Button>
+              </Stack>
+            </Center>
+            <Center>
+              <Box style={styles.sectionWrapper} mb={5}>
+                {/* Submit button */}
+                <Button
+                  style={styles.button}
+                  alignSelf="center"
+                  mb={2}
+                  onPress={handleSubmit(onSubmit)}
+                >
+                  Lưu thông tin hồ câu
+                </Button>
+                <Button
+                  style={styles.button}
+                  variant="outline"
+                  alignSelf="center"
+                >
+                  Xoá hồ câu
+                </Button>
+              </Box>
+            </Center>
+          </VStack>
+        </FormProvider>
       </ScrollView>
     </>
   );
