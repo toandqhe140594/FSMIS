476521d3a99876012f67c96866c1ba63bb2f6257
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-21 23:16:51 +0700

    Edit axios instance

--- src/utilities/Http.js
+++ src/utilities/Http.js
@@ -6,17 +6,7 @@ const http = axios.create({
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwOTYzMzcyNzI3IiwiaWF0IjoxNjM0NzAxMTkxLCJleHAiOjE2MzQ3ODc1OTF9.1tjdZKLGctxorCXi3NOAkZVN6FhLVvCRoGdpNxqQh1U",
   },
 });
-
-// Set JSON Web Token in Client to be included in all calls
-export const setClientToken = (token) => {
-  http.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  });
-};
 
 export default http;
