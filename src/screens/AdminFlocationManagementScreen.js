aa604705f8dd35998b1d4f49b2cfe35b40d32039
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-22 12:14:08 +0700

    Draft AdminFLocationManagementScreen

---
+++ src/screens/AdminFlocationManagementScreen.js
@@ -0,0 +1,123 @@
+import { Select } from "native-base";
+import React, { useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import InputComponent from "../components/common/InputComponent";
+import FLocationCard from "../components/FLocationCard";
+import HeaderTab from "../components/HeaderTab";
+
+const styles = StyleSheet.create({
+  wrapper: { width: "90%", marginTop: 12 },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  card: {
+    marginTop: 8,
+  },
+});
+
+const APIList = [
+  {
+    id: "spot1",
+    address: "Hưng Yên",
+    image: "https://wallpaperaccess.com/full/317501.jpg",
+    isVerifed: true,
+    name: "Ho cau thuan viet",
+    rate: 4,
+  },
+  {
+    id: "spot2",
+    address: "Hưng Yên",
+    image: "https://wallpaperaccess.com/full/317501.jpg",
+    isVerifed: true,
+    name: "Ho cau thuan viet",
+    rate: 4,
+  },
+  {
+    id: "spot3",
+    address: "Hưng Yên",
+    image: "https://wallpaperaccess.com/full/317501.jpg",
+    isVerifed: false,
+    name: "Ho cau thuan viet",
+    rate: 4,
+  },
+  {
+    id: "spot4",
+    address: "Hưng Yên",
+    image: "https://wallpaperaccess.com/full/317501.jpg",
+    isVerifed: true,
+    name: "Ho cau thuan viet",
+    rate: 4,
+  },
+  {
+    id: "spot5",
+    address: "Hưng Yên",
+    image: "https://wallpaperaccess.com/full/317501.jpg",
+    isVerifed: false,
+    name: "Ho cau thuan viet",
+    rate: 4,
+  },
+];
+
+const AdminFlocationManagementScreen = () => {
+  const [fLocationList, setFLocationList] = useState(APIList);
+  const [filter, setFilter] = useState("Tất cả");
+  const methods = useForm();
+  useEffect(() => {
+    const getFilteredList = () => {
+      switch (filter) {
+        case "Tất cả":
+          return APIList;
+        case "Chưa xác thực":
+          return APIList.filter(({ isVerifed }) => !isVerifed);
+        case "Đã xác thực":
+          return APIList.filter(({ isVerifed }) => isVerifed);
+        default:
+          return fLocationList;
+      }
+    };
+    setFLocationList(getFilteredList());
+  }, [filter]);
+  const renderItem = ({ item }) => (
+    <View style={styles.card}>
+      <FLocationCard {...item} showImage={false} />
+    </View>
+  );
+  return (
+    <>
+      <HeaderTab name="Quản lý xác thực Điểm câu" />
+      <View style={styles.center}>
+        <FormProvider {...methods}>
+          <InputComponent
+            myStyles={styles.wrapper}
+            placeholder="Tìm kiếm theo tên hồ"
+            controllerName="fLocationName"
+          />
+        </FormProvider>
+        <View style={styles.wrapper}>
+          <Select
+            placeholder="Lọc danh sách"
+            fontSize="md"
+            onValueChange={setFilter}
+            value={filter}
+          >
+            <Select.Item label="Tất cả" value="Tất cả" />
+            <Select.Item label="Chưa xác thực" value="Chưa xác thực" />
+            <Select.Item label="Đã xác thực" value="Đã xác thực" />
+          </Select>
+        </View>
+        <FlatList
+          style={styles.wrapper}
+          data={fLocationList}
+          renderItem={renderItem}
+        />
+      </View>
+    </>
+  );
+};
+
+export default AdminFlocationManagementScreen;
