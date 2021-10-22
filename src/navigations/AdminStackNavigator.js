1d2685f881ff7381f33af68ea2bf8ffe8a3e0a11
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-22 17:13:31 +0700

    Add navigation for admin

--- src/navigations/AdminStackNavigator.js
+++ src/navigations/AdminStackNavigator.js
@@ -8,7 +8,10 @@ import AdminFishEditScreen from "../screens/AdminFishEditScreen";
 import AdminFishingMethodEditScreen from "../screens/AdminFishingMethodEditScreen";
 import AdminFishingMethodManagementScreen from "../screens/AdminFishingMethodManagementScreen";
 import AdminFishManagementScreen from "../screens/AdminFishManagementScreen";
+import AdminFLocationReportDetailScreen from "../screens/AdminFLocationReportDetailScreen";
 import AdminMainScreen from "../screens/AdminMainScreen";
+import AdminReportEventPostScreen from "../screens/AdminReportEventPostScreen";
+import AdminReportReviewDetailScreen from "../screens/AdminReportReviewDetailScreen";
 
 const PlaceholderScreen = () => {
   return <></>;
@@ -53,6 +56,19 @@ const AdminStackNavigator = () => {
         component={AdminFishingMethodEditScreen}
       />
 
+      <AdminStack.Screen
+        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_LOCATION_DETAIL}
+        component={AdminFLocationReportDetailScreen}
+      />
+      <AdminStack.Screen
+        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_POST_DETAIL}
+        component={AdminReportEventPostScreen}
+      />
+      <AdminStack.Screen
+        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_REVIEW_DETAIL}
+        component={AdminReportReviewDetailScreen}
+      />
+
       {/* Placeholder */}
       <AdminStack.Screen
         name={ROUTE_NAMES.ADMIN_LOCATION_MANAGEMENT}
@@ -64,18 +80,6 @@ const AdminStackNavigator = () => {
       />
       <AdminStack.Screen
         name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT}
-        component={PlaceholderScreen}
-      />
-      <AdminStack.Screen
-        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_LOCATION_DETAIL}
-        component={PlaceholderScreen}
-      />
-      <AdminStack.Screen
-        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_POST_DETAIL}
-        component={PlaceholderScreen}
-      />
-      <AdminStack.Screen
-        name={ROUTE_NAMES.ADMIN_REPORT_MANAGEMENT_REVIEW_DETAIL}
         component={PlaceholderScreen}
       />
     </AdminStack.Navigator>
