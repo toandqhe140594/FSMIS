d866d326dda7b3cc4fc789be0ca5bb53d26e3c2c
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-22 16:20:27 +0700

    Create constants container files

---
+++ src/constants/api.js
@@ -0,0 +1,3 @@
+export const PERSONAL_CHECKIN = "personal/checkin";
+export const PERSONAL_CATCH_REPORT = "personal/catch";
+export const PERSONAL_INFORMATION = "personal";
---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+import * as apis from "./api";
+import * as routes from "./route";
+
+export const ROUTE_NAMES = {
+  ...routes,
+};
+export const API_URL = {
+  ...apis,
+};
+
+export const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+export const DEFAULT_LATLNG = {
+  latitude: 21.05113180001943,
+  longitude: 105.82025917733947,
+};
+export const AUTH_TOKEN = "AuthToken";
