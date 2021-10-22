f7b5bc0292a0b172c8e4f3b30f5249d16460b9e2
ToanDQ <toandqhe140594@fpt.edu.vn>
2021-10-22 23:27:37 +0700

    Api url for saved locations and location avg score

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,10 @@ export const AUTHENTICATION_REGISTER = "auth/register";
 
 export const PERSONAL_CHECKIN = "personal/checkin";
 export const PERSONAL_CATCH_REPORT = "personal/catch";
+export const PERSONAL_SAVED_LOCATION = "personal/save";
 export const PERSONAL_INFORMATION = "personal";
 
 export const LOCATION_OVERVIEW = "location";
 export const LOCATION_NEARBY = "location/nearby";
+export const LOCATION_LAKE_ALL = "lake";
+export const LOCATION_REVIEW_SCORE = "review/score";
