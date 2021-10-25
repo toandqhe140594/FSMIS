4ae2098b9a2279b0a3796b6e5d5d0eac48e77a02
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-10-25 22:46:15 +0700

    Add api url for get personal notifications

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,6 +4,7 @@ export const AUTHENTICATION_REGISTER = "auth/register";
 export const PERSONAL_CHECKIN = "personal/checkin";
 export const PERSONAL_CATCH_REPORT = "personal/catch";
 export const PERSONAL_SAVED_LOCATION = "personal/save";
+export const PERSONAL_NOTIFICATION = "personal/notification";
 export const PERSONAL_INFORMATION = "personal";
 
 export const LOCATION_OVERVIEW = "location";
