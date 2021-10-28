6a7246382f541bc9be77a217982d3e39e2ce336d
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-10-28 17:29:13 +0700

    [FManage - Screen] Replace AddImageButton with custom Pressable

--- src/screens/FManageSelectScreen.js
+++ src/screens/FManageSelectScreen.js
@@ -1,8 +1,8 @@
-import { Box, Center, ScrollView, VStack } from "native-base";
+import { VStack } from "native-base";
 import React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 
-import AddImageButton from "../components/common/AddImageButton";
-import SpotCard from "../components/FLocationCard";
+import FLocationCard from "../components/FLocationCard";
 import HeaderTab from "../components/HeaderTab";
 
 const spotExample = [
@@ -22,31 +22,23 @@ const spotExample = [
     name: "Ho cau thuan viet",
     rate: 4,
   },
-  {
-    id: 3,
-    address: "Hưng Yên",
-    image: "https://wallpaperaccess.com/full/317501.jpg",
-    isVerifed: true,
-    name: "Ho cau thuan viet",
-    rate: 4,
-  },
-  {
-    id: 4,
-    address: "Hưng Yên",
-    image: "https://wallpaperaccess.com/full/317501.jpg",
-    isVerifed: true,
-    name: "Ho cau thuan viet",
-    rate: 4,
-  },
-  {
-    id: 5,
-    address: "Hưng Yên",
-    image: "https://wallpaperaccess.com/full/317501.jpg",
-    isVerifed: true,
-    name: "Ho cau thuan viet",
-    rate: 4,
-  },
 ];
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: "center",
+  },
+  border: {
+    width: 110,
+    height: 110,
+    marginTop: 8,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderStyle: "dashed",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 const FlocationSelectorScreen = () => {
   // Center the add button if the list is emtpy
@@ -56,19 +48,22 @@ const FlocationSelectorScreen = () => {
   return (
     <>
       <HeaderTab name="Chọn điểm câu làm việc" />
-      <ScrollView _contentContainerStyle={getEmptyListStyling()}>
-        <Center style={getEmptyListStyling()}>
-          <Box style={{ marginTop: 1 }}>
-            <AddImageButton />
-          </Box>
+      <ScrollView contentContainerStyle={getEmptyListStyling()}>
+        <View style={[styles.center, getEmptyListStyling()]}>
+          <Pressable onPress={() => {}}>
+            <View style={styles.border}>
+              <Text style={{ fontSize: 12 }}>Thêm điểm câu</Text>
+            </View>
+          </Pressable>
+
           {spotExample.length > 0 && (
             <VStack w="90%" space={2} my={2}>
               {spotExample.map((spot) => (
-                <SpotCard {...spot} isManaged key={spot.id} />
+                <FLocationCard {...spot} isManaged key={spot.id} />
               ))}
             </VStack>
           )}
-        </Center>
+        </View>
       </ScrollView>
     </>
   );
