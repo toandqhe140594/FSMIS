40475c713894b7659824273d5305aa89ff17df57
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-10-29 18:07:32 +0700

    Add location picker for flocation edit screen

--- src/components/FLocationEditProfile/MapOverviewBox.js
+++ src/components/FLocationEditProfile/MapOverviewBox.js
@@ -1,15 +1,84 @@
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { useStoreState } from "easy-peasy";
 import { Box } from "native-base";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+import { DEFAULT_LATLNG } from "../../constants";
+import { goToFManageLocationPickScreen } from "../../navigations";
 
 const styles = StyleSheet.create({
-  container: { borderColor: "black", borderWidth: 1 },
+  mapviewContainer: { borderColor: "black", borderWidth: 1 },
 });
 
 const MapOverviewBox = () => {
+  const navigation = useNavigation();
+
+  const mapRef = useRef(null);
+
+  const locationLatLng = useStoreState(
+    (states) => states.FManageModel.locationLatLng,
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      // If the map reference is inited and locationLatLng is not empty
+      if (mapRef.current && locationLatLng)
+        mapRef.current.animateToRegion(
+          {
+            latitude: locationLatLng.latitude,
+            longitude: locationLatLng.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          },
+          1,
+        );
+      return () => {};
+    }, [locationLatLng]),
+  );
+
   return (
-    <TouchableOpacity>
-      <Box h={20} style={styles.container} />
+    <TouchableOpacity
+      onPress={() => {
+        goToFManageLocationPickScreen(navigation);
+      }}
+    >
+      <Box h="150" style={styles.mapviewContainer}>
+        {!locationLatLng.latitude && (
+          <MapView
+            ref={mapRef}
+            initialRegion={{
+              latitude: DEFAULT_LATLNG.latitude,
+              longitude: DEFAULT_LATLNG.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            style={{ height: "100%", width: "100%" }}
+            liteMode
+          />
+        )}
+        {locationLatLng.latitude && (
+          <MapView
+            ref={mapRef}
+            initialRegion={{
+              latitude: locationLatLng.latitude,
+              longitude: locationLatLng.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            style={{ height: "100%", width: "100%" }}
+            liteMode
+          >
+            <Marker
+              coordinate={{
+                latitude: locationLatLng.latitude,
+                longitude: locationLatLng.longitude,
+              }}
+            />
+          </MapView>
+        )}
+      </Box>
     </TouchableOpacity>
   );
 };
