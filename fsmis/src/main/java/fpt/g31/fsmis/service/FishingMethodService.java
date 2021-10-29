714ccc917eaafbe9bfb1387a005a54e82ac4d3e1
MoeO17 <anhtdhe140447@fpt.edu.vn>
2021-10-29 17:53:42 +0700

    fix get all method and fish species

--- fsmis/src/main/java/fpt/g31/fsmis/service/FishingMethodService.java
+++ fsmis/src/main/java/fpt/g31/fsmis/service/FishingMethodService.java
@@ -1,8 +1,13 @@
 package fpt.g31.fsmis.service;
 
+import fpt.g31.fsmis.dto.output.FishingMethodDtoOut;
+import fpt.g31.fsmis.entity.FishingMethod;
 import fpt.g31.fsmis.repository.FishingMethodRepos;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -10,7 +15,17 @@ public class FishingMethodService {
 
     private final FishingMethodRepos fishingMethodRepos;
 
-    public Object getAll() {
-        return fishingMethodRepos.findAll();
+    public List<FishingMethodDtoOut> getAll() {
+        List<FishingMethod> fishingMethods = fishingMethodRepos.findAllByActiveIsTrue();
+        List<FishingMethodDtoOut> fishingMethodDtoOuts = new ArrayList<>();
+        for (FishingMethod fishingMethod :
+                fishingMethods) {
+            FishingMethodDtoOut fishingMethodDtoOut = FishingMethodDtoOut.builder()
+                    .id(fishingMethod.getId())
+                    .name(fishingMethod.getName())
+                    .build();
+            fishingMethodDtoOuts.add(fishingMethodDtoOut);
+        }
+        return fishingMethodDtoOuts;
     }
 }
