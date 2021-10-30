b84f479142228fd2dccb2e4152fbcbe7b9d3ecdc
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-10-30 22:59:34 +0700

    Connect api to get list of staff

--- src/screens/FManageEmployeeManagementScreen.js
+++ src/screens/FManageEmployeeManagementScreen.js
@@ -1,52 +1,67 @@
+import { useNavigation } from "@react-navigation/native";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import { Box, Button, Center } from "native-base";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 import { Divider } from "react-native-elements";
 
 import EmployeeCard from "../components/EmployeeCard";
 import HeaderTab from "../components/HeaderTab";
-
-const data = [
-  {
-    id: "1",
-    name: "Đào Quốc Toản",
-    phoneNumber: "098765432",
-  },
-  {
-    id: "2",
-    name: "Nguyễn Đình Đạt",
-    phoneNumber: "098765432",
-    image: "https://picsum.photos/200",
-  },
-  {
-    id: "3",
-    name: "Nguyễn Hoàng Đức32323dsdsdsdsdsabcd",
-    phoneNumber: "098765432",
-    image: "https://picsum.photos/200",
-  },
-];
+import { goToFManageAddStaffScreen } from "../navigations";
 
 const FManageEmployeeManagementScreen = () => {
-  const [employeeListData, setEmployeeListData] = useState(data);
-  const deleteEmployee = (id) => {
-    setEmployeeListData(
-      employeeListData.filter((employee) => employee.id !== id),
-    );
+  const navigation = useNavigation();
+  const listOfStaff = useStoreState(
+    (states) => states.FManageModel.listOfStaff,
+  );
+  const getListOfStaff = useStoreActions(
+    (actions) => actions.FManageModel.getListOfStaff,
+  );
+  const [displayedList, setDisplayedList] = useState(listOfStaff);
+
+  const deleteStaff = (id) => {
+    setDisplayedList(displayedList.filter((employee) => employee.id !== id));
   };
+
+  useEffect(() => {
+    getListOfStaff();
+  }, []);
+
+  useEffect(() => {
+    setDisplayedList(listOfStaff);
+  }, [listOfStaff]);
+
   return (
     <>
       <HeaderTab name="Quản lý nhân viên" />
       <Box flex={1} alignItems="center">
         <Center w="80%" my={5}>
-          <Button>Thêm nhân viên</Button>
+          <Button
+            onPress={() => {
+              goToFManageAddStaffScreen(navigation);
+            }}
+          >
+            Thêm nhân viên
+          </Button>
         </Center>
         <Box flex={1} w="100%">
           <FlatList
-            data={employeeListData}
-            renderItem={({ item }) => (
-              <EmployeeCard employee={item} deleteEmployee={deleteEmployee} />
-            )}
-            keyExtractor={(item) => item.id}
+            data={displayedList}
+            renderItem={({ item }) => {
+              const { id, name, phone, avatar } = item;
+              return (
+                <EmployeeCard
+                  employee={{
+                    id,
+                    name,
+                    phoneNumber: phone,
+                    image: avatar,
+                  }}
+                  deleteEmployee={deleteStaff}
+                />
+              );
+            }}
+            keyExtractor={(item) => item.id.toString()}
             ItemSeparatorComponent={Divider}
             ListHeaderComponent={Divider}
             ListFooterComponent={Divider}
