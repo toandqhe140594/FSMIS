12d058cd608a47713200672dd4b859b23b1d5f5b
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-01 21:25:33 +0700

    Implement API for write report action

--- src/screens/WriteReportScreen.js
+++ src/screens/WriteReportScreen.js
@@ -1,10 +1,65 @@
-import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useStoreActions } from "easy-peasy";
+import React, { useEffect, useState } from "react";
+import { Text, TextInput, ToastAndroid, View } from "react-native";
 import { Button, Divider } from "react-native-elements";
 
 import HeaderTab from "../components/HeaderTab";
+import { goBack } from "../navigations";
 
 const ReportScreen = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const [reportParams, setReportParams] = useState({});
+  const [success, setSuccess] = useState(false);
+  const [content, setContent] = useState("");
+
+  const writeNewReport = useStoreActions(
+    (actions) => actions.ProfileModel.writeNewReport,
+  );
+
+  const onSubmit = () => {
+    const trimmedContent = content.trim();
+    // If the content is empty or only contains blank characters
+    if (!trimmedContent) {
+      ToastAndroid.showWithGravityAndOffset(
+        "Nội đung báo cáo không thể bỏ trống",
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50,
+      );
+    }
+    writeNewReport({
+      id: reportParams.id,
+      type: reportParams.type,
+      content: trimmedContent,
+      setSuccess,
+    });
+  };
+
+  useEffect(() => {
+    // If write report success, show toast
+    if (success) {
+      ToastAndroid.showWithGravityAndOffset(
+        "Báo cáo thành công",
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+        25,
+        50,
+      );
+      goBack(navigation);
+    }
+  }, [success]);
+
+  useEffect(() => {
+    // If there are params pass through
+    if (route.params.id) {
+      const { id, type } = route.params;
+      setReportParams({ id, type });
+    }
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <HeaderTab name="Báo cáo vi phạm" />
@@ -35,9 +90,16 @@ const ReportScreen = () => {
               textAlignVertical: "top",
               padding: 5,
             }}
+            onChangeText={setContent}
           />
         </View>
-        <Button containerStyle={{ margin: "10%" }} title="Báo cáo" />
+        <Button
+          containerStyle={{ margin: "10%" }}
+          title="Báo cáo"
+          onPress={() => {
+            onSubmit();
+          }}
+        />
       </View>
     </View>
   );
