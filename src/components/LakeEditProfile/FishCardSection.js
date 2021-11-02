ed6f0731d7ca8d2f18bae0e1e9c5dc87523831aa
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-02 10:07:03 +0700

    [FManage - Screen & Component] Implement useFieldArray and yup

---
+++ src/components/LakeEditProfile/FishCardSection.js
@@ -0,0 +1,126 @@
+import { Button, Input, Select } from "native-base";
+import React, { useEffect } from "react";
+import { Controller, useFieldArray, useFormContext } from "react-hook-form";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginVertical: 5,
+    backgroundColor: "#fafafa",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  rowWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 8,
+  },
+  error: { color: "#f43f5e", fontSize: 12, fontStyle: "italic" },
+});
+
+const FishCardSection = () => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "cards",
+    // Cards will be unregistered when unmount
+    shouldUnregister: true,
+  });
+  /**
+   * Append a card ready to use
+   */
+  useEffect(() => {
+    const initCard = () => append({});
+    initCard();
+  }, []);
+  return (
+    <>
+      {/* fields controls each object with field fishType, amount, totalWeight and isReleased */}
+      {fields.map(({ id }, index) => (
+        <View style={styles.cardWrapper} key={id}>
+          <Controller
+            name={`cards[${index}].fishType`}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Select
+                mt={2}
+                fontSize="md"
+                placeholder="Chọn loại cá"
+                onValueChange={onChange}
+                selectedValue={value}
+              >
+                <Select.Item label="Cá diếc" value={1} />
+                <Select.Item label="Cá chép" value={2} />
+              </Select>
+            )}
+          />
+          {/* Check error message of fishType field of a specific object in the fieldArray */}
+          {errors.cards?.[index]?.fishType?.message && (
+            <Text style={styles.error}>
+              {errors?.cards?.[index].fishType?.message}
+            </Text>
+          )}
+          <Controller
+            name={`cards[${index}].amount`}
+            control={control}
+            render={({ field: { value, onChange, onBlur } }) => (
+              <Input
+                mt={2}
+                fontSize="md"
+                placeholder="Nhập số cá (con)"
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+              />
+            )}
+          />
+          {/* Check error message of amount feld of a specific object in the fieldArray */}
+          {errors.cards?.[index]?.amount?.message && (
+            <Text style={styles.error}>
+              {errors.cards?.[index].amount?.message}
+            </Text>
+          )}
+          <Controller
+            name={`cards[${index}].totalWeight`}
+            control={control}
+            render={({ field: { value, onChange, onBlur } }) => (
+              <Input
+                mt={2}
+                fontSize="md"
+                placeholder="Tổng cân nặng (kg)"
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+              />
+            )}
+          />
+          {/* Check error message of totalWeight field of a specific object in the fieldArray */}
+          {errors.cards?.[index]?.totalWeight?.message && (
+            <Text style={styles.error}>
+              {errors.cards?.[index].totalWeight?.message}
+            </Text>
+          )}
+          <View style={styles.rowWrapper}>
+            <Button fontSize="md" w="40%" onPress={() => remove(index)}>
+              Xoá
+            </Button>
+          </View>
+        </View>
+      ))}
+      <Button w="90%" mt={3} alignSelf="center" onPress={() => append({})}>
+        Thêm thẻ
+      </Button>
+    </>
+  );
+};
+
+export default FishCardSection;
--- src/screens/FManageLakeEditProfileScreen.js
+++ src/screens/FManageLakeEditProfileScreen.js
@@ -2,7 +2,11 @@
 // import "react-native-get-random-values";
 
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useFocusEffect, useRoute } from "@react-navigation/native";
+import {
+  useFocusEffect,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
 import {
   Box,
   Button,
@@ -22,8 +26,8 @@ import InputComponent from "../components/common/InputComponent";
 import MultiImageSection from "../components/common/MultiImageSection";
 import TextAreaComponent from "../components/common/TextAreaComponent";
 import HeaderTab from "../components/HeaderTab";
-import AddFishCard from "../components/LakeEditProfile/AddFishCard";
 import CheckboxSelectorComponent from "../components/LakeEditProfile/CheckboxSelectorComponent";
+import FishCardSection from "../components/LakeEditProfile/FishCardSection";
 import { ROUTE_NAMES } from "../constants";
 
 const validationSchema = yup.object().shape({
@@ -39,15 +43,20 @@ const validationSchema = yup.object().shape({
   lakeLength: yup.string().required("Chiều dài hồ không được để trống"),
   lakeWidth: yup.string().required("Chiều rộng hồ không được để trống"),
   lakeDepth: yup.string().required("Độ sâu của hồ không được để trống"),
+  cards: yup.array().of(
+    yup.object().shape({
+      fishType: yup.number().required("Loại cá không được để trống"),
+      amount: yup.number().required("Số cá bắt được không được để trống"),
+      totalWeight: yup
+        .number()
+        .required("Tổng cân nặng cá không được để trống"),
+      minWeight: yup.number(),
+      maxWeight: yup.number(),
+    }),
+  ),
 });
 
 const fishingMethodData = ["Câu đài", "Câu đơn", "Câu lục"];
-
-let itemKey = 0;
-const generateKey = () => {
-  itemKey += 1;
-  return `${itemKey}`;
-};
 
 const styles = StyleSheet.create({
   sectionWrapper: {
@@ -60,15 +69,7 @@ const styles = StyleSheet.create({
 
 const LakeEditProfileScreen = () => {
   const route = useRoute();
-
-  const initFishCard = {
-    id: generateKey(),
-    fish: "",
-    weightDescription: "",
-    amount: "",
-    totalWeight: "",
-  };
-  const [cardList, setCardList] = useState([initFishCard]);
+  const navigation = useNavigation();
   const [imageArray, setImageArray] = useState([]);
   const methods = useForm({
     mode: "onSubmit",
@@ -80,28 +81,16 @@ const LakeEditProfileScreen = () => {
   const onSubmit = (data) => {
     // Test submit
     console.log(data);
-    console.log(cardList);
   };
 
-  const addCard = () => {
-    const newCard = initFishCard;
-    setCardList((prev) => [...prev, newCard]);
-  };
-  const deleteCard = (id) => {
-    const newCardList = cardList.filter((card) => card.id !== id);
-    setCardList(newCardList);
-  };
-  const updateCard = (id, name, value) => {
-    const newCardList = cardList.map((card) => {
-      if (card.id === id) {
-        return { ...card, [name]: value };
-      }
-      return card;
-    });
-    setCardList(newCardList);
-  };
+  /**
+   * Remove an image and update the image array
+   * @param {number} id: id of the deleted image
+   */
   const updateImageArray = (id) => {
-    setImageArray(imageArray.filter((image) => image.id !== id));
+    const newBase64Array = imageArray.filter((image) => image.id !== id);
+    setImageArray(newBase64Array);
+    navigation.setParams({ base64Array: newBase64Array });
   };
 
   // Fire when navigates back to the screen
@@ -191,23 +180,7 @@ const LakeEditProfileScreen = () => {
                 <Text fontSize="md" bold>
                   Các loại cá
                 </Text>
-                <VStack mb={1}>
-                  {cardList.map((card) => (
-                    <AddFishCard
-                      key={card.id}
-                      id={card.id}
-                      deleteCard={deleteCard}
-                      updateCard={updateCard}
-                    />
-                  ))}
-                </VStack>
-                <Button
-                  style={styles.button}
-                  alignSelf="center"
-                  onPress={addCard}
-                >
-                  Thêm loại cá
-                </Button>
+                <FishCardSection />
               </Stack>
             </Center>
             <Center>
