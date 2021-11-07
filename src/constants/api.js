9671c34b73eaae057f8e087b3f3820e402e57910
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-07 18:28:59 +0700

    Implement action to verify pending catch reports

--- src/constants/api.js
+++ src/constants/api.js
@@ -33,6 +33,7 @@ export const LOCATION_REVIEW_PERSONAL = "review/me";
 export const LOCATION_REVIEW_PERSONAL_DELETE = "review/me/delete";
 export const LOCATION_REVIEW_PERSONAL_POST = "review/me/post";
 export const LOCATION_CATCH_REPORT_UNRESOLVED = "catch/pending";
+export const LOCATION_CATCH_REPORT_APPROVE = "catches/approve";
 
 export const ADDRESS_ALL_PROVINCE = "address/province";
 export const ADDRESS_PROVINCE_DISTRICT = "address/district";
