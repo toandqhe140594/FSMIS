7c0ff82e9a85dca209612b641a09ff52e292ee0c
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-09 17:19:40 +0700

    Add badge to show open/close status

--- src/components/FLocationCard.js
+++ src/components/FLocationCard.js
@@ -3,7 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import { Box, Pressable, Text, VStack } from "native-base";
 import PropTypes from "prop-types";
 import React from "react";
-import { Card } from "react-native-elements";
+import { Badge, Card } from "react-native-elements";
 import { Rating } from "react-native-ratings";
 
 import {
@@ -23,6 +23,7 @@ const SpotCard = ({
   isManaged,
   isAdmin,
   showImage,
+  isClosed,
 }) => {
   const navigation = useNavigation();
 
@@ -41,11 +42,24 @@ const SpotCard = ({
   return (
     <Pressable onPress={() => onPress()}>
       <Card containerStyle={{ width: "100%", padding: 0, margin: 0 }}>
-        {showImage && <Card.Image source={{ uri: image }} />}
+        {showImage && (
+          <Card.Image source={{ uri: image }}>
+            <Badge
+              containerStyle={{ position: "absolute", top: 4, left: 4 }}
+              badgeStyle={{
+                borderRadius: 0,
+                paddingVertical: 10,
+                paddingHorizontal: 8,
+              }}
+              value={isClosed ? "Đóng cửa" : "Mở cửa"}
+              status={isClosed ? "error" : "success"}
+            />
+          </Card.Image>
+        )}
         <VStack mt={1.5} mb={2} ml={3} space={1.5}>
           <Box>
             <Box flexDir="row" alignItems="center">
-              <Text bold fontSize="18" mr={2}>
+              <Text bold fontSize="18" mr={2} isTruncated numberOfLines={1}>
                 {name}
               </Text>
               {isVerifed && (
@@ -61,7 +75,9 @@ const SpotCard = ({
               startingValue={rate}
             />
           </Box>
-          <Text>{address}</Text>
+          <Text isTruncated numberOfLines={1}>
+            {address}
+          </Text>
         </VStack>
       </Card>
     </Pressable>
@@ -79,6 +95,7 @@ SpotCard.propTypes = {
   showImage: PropTypes.bool,
   isAdmin: PropTypes.bool,
   role: PropTypes.string,
+  isClosed: PropTypes.bool,
 };
 SpotCard.defaultProps = {
   image: "https://picsum.photos/200",
@@ -89,6 +106,7 @@ SpotCard.defaultProps = {
   showImage: true,
   isAdmin: false,
   role: "ANGLER",
+  isClosed: false,
 };
 
 export default SpotCard;
