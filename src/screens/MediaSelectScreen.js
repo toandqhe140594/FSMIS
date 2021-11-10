8b2e09b2ad327a7c9753c42c0910ac10bc0898fe
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-11 02:19:06 +0700

    Optimization: resize all image to width 750

--- src/screens/MediaSelectScreen.js
+++ src/screens/MediaSelectScreen.js
@@ -90,11 +90,11 @@ const MediaSelectScreen = () => {
     [],
   );
 
-  // Manage selected iamge result
+  // Manage selected image result
   const widgetResize = useMemo(
     () => ({
-      width: 200,
-      compress: 0.7,
+      width: 750,
+      compress: 1,
       base64: true,
       saveTo: FILE_EXTENSION,
     }),
