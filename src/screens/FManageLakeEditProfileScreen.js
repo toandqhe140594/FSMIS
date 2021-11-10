53619eec76885a18141c15361e72b9f192f8a537
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-10 20:27:20 +0700

    [FManage - LakeEdit] Remove left over of setShowOverlay

--- src/screens/FManageLakeEditProfileScreen.js
+++ src/screens/FManageLakeEditProfileScreen.js
@@ -71,7 +71,7 @@ const LakeEditProfileScreen = () => {
    * @param {Object} data data from controller
    */
   const onSubmit = (data) => {
-    setShowOverlay(true);
+    setIsLoading(true);
     const { id } = lakeDetail;
     const imageUrl = imageArray[0].base64;
     const updateData = { ...data, imageUrl };
