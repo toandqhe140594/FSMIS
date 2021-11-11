cc0a8e96e9f2bd6e9546de0c966102dc0dd2396e
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-11 17:31:09 +0700

    [Admin - AddEditFish] Implement MultiImageSection and custom input

--- src/screens/AdminFishEditScreen.js
+++ src/screens/AdminFishEditScreen.js
@@ -1,12 +1,25 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useRoute } from "@react-navigation/native";
-import { Box, Button, Center, Input, Text } from "native-base";
-import React, { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { Avatar } from "react-native-elements";
+import {
+  useFocusEffect,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import { Box, Button, VStack } from "native-base";
+import React, { useCallback, useEffect, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Dimensions } from "react-native";
 import * as yup from "yup";
 
+// DucHM ADD_START 11/11/2021
+import InputComponent from "../components/common/InputComponent";
+import MultiImageSection from "../components/common/MultiImageSection";
 import HeaderTab from "../components/HeaderTab";
+import { ROUTE_NAMES, SCHEMA } from "../constants";
+// DucHM ADD_END 11/11/2021
+
+const OFFSET_BOTTOM = 85;
+// Get window height without status bar height
+const CUSTOM_SCREEN_HEIGHT = Dimensions.get("window").height - OFFSET_BOTTOM;
 
 // Validation schema for form
 const validationSchema = yup.object().shape({
@@ -14,20 +27,20 @@ const validationSchema = yup.object().shape({
 });
 const AdminFishEditScreen = () => {
   const route = useRoute();
-
+  const navigation = useNavigation();
   const [isNew, setIsNew] = useState(true);
-  const [imageData, setImageData] = useState("");
+  const [imageData, setImageData] = useState([]);
 
-  const {
-    control,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm({
-    mode: "all",
+  const methods = useForm({
+    mode: "onSubmit",
     resolver: yupResolver(validationSchema),
   });
-
+  const { handleSubmit, setValue } = methods;
+  // DucHM ADD_START 11/11/2021
+  const updateImageArray = (id) => {
+    setImageData(imageData.filter((image) => image.id !== id));
+  };
+  // DucHM ADD_END 11/11/2021
   const onSubmit = (data) => {
     console.log(data); // Test only
   };
@@ -37,61 +50,59 @@ const AdminFishEditScreen = () => {
     if (id) {
       const { name, image } = route.params;
       setIsNew(false);
-      setImageData(image);
+      setImageData([{ id: 1, base64: image }]);
       setValue("fishName", name);
     }
   }, []);
-
+  // DucHM ADD_START 11/11/2021
+  useFocusEffect(
+    // useCallback will listen to route.param
+    useCallback(() => {
+      if (route.params?.base64Array && route.params.base64Array.length) {
+        setImageData(route.params?.base64Array);
+        navigation.setParams({ base64Array: [] });
+      }
+    }, [route.params]),
+  );
+  // DucHM ADD_END 11/11/2021
   return (
     <>
       <HeaderTab name="Quản lý loại cá" />
-      <Box flex={1} alignItems="center" justifyContent="flex-start">
-        <Center w="80%">
-          <Avatar
-            size="xlarge"
-            source={{
-              uri: imageData || "https://picsum.photos/200",
-            }}
-            containerStyle={{ padding: 10, margin: 5 }}
-            imageProps={{
-              resizeMode: "contain",
-            }}
-          />
-
-          {/* Phone number input field */}
-          <Box alignItems="flex-start" w="100%">
-            <Text bold fontSize="md">
-              Tên cá <Text color="danger.500">*</Text>
-            </Text>
-            <Controller
-              control={control}
-              name="fishName"
-              render={({ field: { onChange, onBlur, value } }) => (
-                <Input
-                  placeholder="Tên loại cá"
-                  size="md"
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                  w="100%"
-                  h={10}
-                  mt={2}
-                />
-              )}
+      <Box height={CUSTOM_SCREEN_HEIGHT} borderWidth={1}>
+        <FormProvider {...methods}>
+          <VStack
+            w="100%"
+            flexGrow={1}
+            flexBasis={1}
+            justifyContent="flex-start"
+            alignItems="center"
+            space={2}
+          >
+            {/* DucHM ADD_START 11/11/2021 */}
+            <MultiImageSection
+              containerStyle={{ width: "90%" }}
+              formRoute={ROUTE_NAMES.ADMIN_FISH_MANAGEMENT_EDIT}
+              imageArray={imageData}
+              deleteImage={updateImageArray}
             />
-            {errors.fishName?.message && (
-              <Text color="error.500" fontSize="xs" italic>
-                {errors.fishName?.message}
-              </Text>
-            )}
-          </Box>
-        </Center>
+            <InputComponent
+              myStyles={{ width: "90%" }}
+              label="Tên cá"
+              hasAsterisk
+              placeholder="Nhập tên cá"
+              controllerName="fishName"
+            />
+            {/* DucHM ADD_END 11/11/2021 */}
+          </VStack>
 
-        <Box w="70%" mb={5} justifyContent="flex-end" flex={1}>
-          <Button w="100%" onPress={handleSubmit(onSubmit)}>
+          <Button
+            w="100%"
+            alignSelf="flex-end"
+            onPress={handleSubmit(onSubmit)}
+          >
             {isNew ? "Thêm loại cá" : "Lưu thay đổi"}
           </Button>
-        </Box>
+        </FormProvider>
       </Box>
     </>
   );
