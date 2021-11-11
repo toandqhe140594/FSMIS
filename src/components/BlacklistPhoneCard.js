9a87fec5d1c99bcb6aa65377327eea4275816e14
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-12 00:03:16 +0700

    Add an alert box that show detail description

--- src/components/BlacklistPhoneCard.js
+++ src/components/BlacklistPhoneCard.js
@@ -3,7 +3,11 @@ import { Box, Button, Text } from "native-base";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 
-import { showAlertConfirmBox, showToastMessage } from "../utilities";
+import {
+  showAlertBox,
+  showAlertConfirmBox,
+  showToastMessage,
+} from "../utilities";
 import PressableCustomCard from "./PressableCustomCard";
 
 const BlacklistPhoneCard = ({ phone, description }) => {
@@ -12,6 +16,17 @@ const BlacklistPhoneCard = ({ phone, description }) => {
   const whitelistPhoneNumber = useStoreActions(
     (actions) => actions.AccountManagementModel.whitelistPhoneNumber,
   );
+
+  const showDetailInformation = () => {
+    // If the phone in blacklist has description aka reason
+    if (description)
+      // Show an alert box that show detail of the description
+      showAlertBox(
+        `Số điện thoại: ${phone}`,
+        `Lý do bị đưa vào danh sách đen:\n${description}`,
+        "Đóng",
+      );
+  };
 
   const whitelistPhoneFunction = () => {
     whitelistPhoneNumber({ phone, setSuccess });
@@ -32,7 +47,7 @@ const BlacklistPhoneCard = ({ phone, description }) => {
   }, [success]);
 
   return (
-    <PressableCustomCard onPress={() => {}}>
+    <PressableCustomCard onPress={showDetailInformation}>
       <Box py={2} px={3} flex={1} flexDir="row">
         <Box flex={2} justifyContent="center">
           <Text bold fontSize="lg">
