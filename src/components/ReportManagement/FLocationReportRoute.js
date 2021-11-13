7585351d56952bd4178d33ebc1dbeb17cfe62ffd
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-13 14:41:16 +0700

    [Component - FLoationReport] Connect api

--- src/components/ReportManagement/FLocationReportRoute.js
+++ src/components/ReportManagement/FLocationReportRoute.js
@@ -1,12 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
-import { useStoreState } from "easy-peasy";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import { Select } from "native-base";
 import React, { useEffect, useState } from "react";
-import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { goToAdminFLocationReportDetailScreen } from "../../navigations";
 import HeaderTab from "../HeaderTab";
-import ReviewReportCard from "./ReportCard";
+import ReportCard from "./ReportCard";
 
 const styles = StyleSheet.create({
   flatList: { marginTop: 12 },
@@ -14,11 +20,15 @@ const styles = StyleSheet.create({
 
 const FLocationReportRoute = () => {
   const navigation = useNavigation();
-  const [filter, setFilter] = useState("");
-  const reportListModel = useStoreState(
-    (states) => states.ReportModel.listLocationReport,
+  const [isLoading, setIsLoading] = useState(true);
+  const [pageNo, setPageNo] = useState(1);
+  const [momentumScrollBegin, setMomentumScrollBegin] = useState(true);
+  // const [filter, setFilter] = useState("");
+  const { listLocationReport } = useStoreState((state) => state.ReportModel);
+  const { getListLocationReportLocation, resetReportList } = useStoreActions(
+    (actions) => actions.ReportModel,
   );
-  const [reportList, setReportList] = useState(reportListModel);
+  // const [reportList, setReportList] = useState(listLocationReport);
   const renderItem = ({ item }) => {
     return (
       <TouchableOpacity
@@ -26,25 +36,60 @@ const FLocationReportRoute = () => {
           goToAdminFLocationReportDetailScreen(navigation);
         }}
       >
-        <ReviewReportCard {...item} isFLocationReport />
+        <ReportCard {...item} isFLocationReport />
       </TouchableOpacity>
     );
   };
+  const renderFooter = () =>
+    isLoading ? <ActivityIndicator size="large" color="#2089DC" /> : <></>;
+  /**
+   * When FlatList scroll to bottom,
+   * process to the next location report page
+   */
+  const handleLoadMore = () => {
+    if (!momentumScrollBegin) {
+      setPageNo(pageNo + 1);
+      setIsLoading(true);
+      setMomentumScrollBegin(true);
+    }
+  };
+  /**
+   * When pageCurrent change, get next location report page
+   */
   useEffect(() => {
-    const getFilteredList = () => {
-      switch (filter) {
-        case "Tất cả":
-          return reportListModel;
-        case "Chưa xử lý":
-          return reportListModel.filter(({ isProcessed }) => !isProcessed);
-        case "Đã xử lý":
-          return reportListModel.filter(({ isProcessed }) => isProcessed);
-        default:
-          return reportList;
-      }
+    getListLocationReportLocation({ pageNo, setIsLoading });
+    return () => {
+      resetReportList({ type: "LOCATION" });
     };
-    setReportList(getFilteredList());
-  }, [filter]);
+  }, [pageNo]);
+  /**
+   * Listen to isLoading state
+   * When the loading stops, set new location report list
+   * to report list state
+   */
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     setReportList(listLocationReport);
+  //   }
+  // }, [isLoading]);
+  /**
+   * Filter list based on select option
+   */
+  // useEffect(() => {
+  //   const getFilteredList = () => {
+  //     switch (filter) {
+  //       case "ALL":
+  //         return reportList;
+  //       case "UNTOUCHED":
+  //         return reportList.filter(({ active }) => !active);
+  //       case "TOUCHED":
+  //         return reportList.filter(({ active }) => active);
+  //       default:
+  //         return reportList;
+  //     }
+  //   };
+  //   setReportList(getFilteredList());
+  // }, [filter]);
   return (
     <>
       <HeaderTab name="Quản lý báo cáo" />
@@ -54,21 +99,26 @@ const FLocationReportRoute = () => {
           my={2}
           alignSelf="center"
           placeholder="Lọc hiển thị báo cáo"
-          defaultValue="Tất cả"
-          value={filter}
-          onValueChange={setFilter}
+          defaultValue="ALL"
+          value="ALL"
+          onValueChange={() => {}}
           fontSize="md"
         >
-          <Select.Item label="Tất cả" value="Tất cả" />
-          <Select.Item label="Chưa xử lý" value="Chưa xử lý" />
-          <Select.Item label="Đã xử lý" value="Đã xử lý" />
+          <Select.Item label="Tất cả" value="ALL" />
+          <Select.Item label="Chưa xử lý" value="UNTOUCHED" />
+          <Select.Item label="Đã xử lý" value="TOUCHED" />
         </Select>
 
         <FlatList
           style={styles.flatList}
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => `${item.id}`}
           renderItem={renderItem}
-          data={reportList}
+          ListFooterComponent={renderFooter}
+          data={listLocationReport}
+          onMomentumScrollBegin={() => {
+            setMomentumScrollBegin(false);
+          }}
+          onEndReached={handleLoadMore}
         />
       </View>
     </>
