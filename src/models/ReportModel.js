c3136346130f899410ebb7a4e5bb4701eafa881d
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-13 14:40:41 +0700

    [Model - ReportModel] Create set, get api for report list

--- src/models/ReportModel.js
+++ src/models/ReportModel.js
@@ -6,101 +6,295 @@ import http from "../utilities/Http";
 const listLocationReport = [
   {
     id: "1",
-    reportTarget: "Hồ Câu Thuần Việt",
-    isProcessed: true,
+    name: "Hồ Câu Thuần Việt",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "2",
-    reportTarget: "Hồ Câu Thuần Việt",
-    isProcessed: true,
+    name: "Hồ Câu Thuần Việt",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "3",
-    reportTarget: "Hồ Câu Thuần Việt",
-    isProcessed: false,
+    name: "Hồ Câu Thuần Việt",
+    active: false,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "4",
-    reportTarget: "Hồ Câu Thuần Việt",
-    isProcessed: true,
+    name: "Hồ Câu Thuần Việt",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "5",
-    reportTarget: "Hồ Câu Thuần Việt",
-    isProcessed: false,
+    name: "Hồ Câu Thuần Việt",
+    active: false,
+    time: "10/11/2021 22:04:08",
   },
 ];
 const listPostReport = [
   {
     id: "1",
-    userName: "Hồ câu Thuần Việt",
+    name: "Hồ câu Thuần Việt",
     postType: "Báo cá",
-    isProcessed: true,
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "2",
-    userName: "Hồ câu Thuần Việt",
+    name: "Hồ câu Thuần Việt",
     postType: "Thông báo",
-    isProcessed: true,
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "3",
-    userName: "Hồ câu Thuần Việt",
+    name: "Hồ câu Thuần Việt",
     postType: "Bồi cá",
-    isProcessed: false,
+    active: false,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "4",
-    userName: "Hồ câu Thuần Việt",
+    name: "Hồ câu Thuần Việt",
     postType: "Báo cá",
-    isProcessed: true,
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "5",
-    userName: "Hồ câu Thuần Việt",
+    name: "Hồ câu Thuần Việt",
     postType: "Báo cá",
-    isProcessed: false,
+    active: false,
+    time: "10/11/2021 22:04:08",
   },
 ];
 
 const listReviewReport = [
   {
     id: "1",
-    reportTarget: "Nguyễn Văn A",
-    isProcessed: true,
+    name: "Nguyễn Văn A",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "2",
-    reportTarget: "Nguyễn Văn A",
-    isProcessed: true,
+    name: "Nguyễn Văn A",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "3",
-    reportTarget: "Nguyễn Văn A",
-    isProcessed: false,
+    name: "Nguyễn Văn A",
+    active: false,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "4",
-    reportTarget: "Nguyễn Văn A",
-    isProcessed: true,
+    name: "Nguyễn Văn A",
+    active: true,
+    time: "10/11/2021 22:04:08",
   },
   {
     id: "5",
-    reportTarget: "Nguyễn Văn A",
-    isProcessed: false,
+    name: "Nguyễn Văn A",
+    active: false,
+    time: "10/11/2021 22:04:08",
+  },
+];
+
+const listCatchReport = [
+  {
+    id: "1",
+    name: "Nguyễn Văn A",
+    active: true,
+  },
+  {
+    id: "2",
+    name: "Nguyễn Văn A",
+    active: true,
+  },
+  {
+    id: "3",
+    name: "Nguyễn Văn A",
+    active: false,
+  },
+  {
+    id: "4",
+    name: "Nguyễn Văn A",
+    active: true,
+  },
+  {
+    id: "5",
+    name: "Nguyễn Văn A",
+    active: false,
   },
 ];
 
 const model = {
-  listLocationReport: [...listLocationReport],
-  listPostReport: [...listPostReport],
-  listReviewReport: [...listReviewReport],
+  listLocationReport: [],
+  listPostReport: [],
+  listReviewReport: [],
+  listCatchReport: [],
+  totalLocationReportPage: 0,
+  totalPostReportPage: 0,
+  totalReviewReportPage: 0,
+  totalCatchReportPage: 0,
 
-  setListReportLocation: action((state, payload) => {
-    state.listReportLocation = payload;
+  /**
+   * Append new location report list to current list
+   * @param {String} [payload.type] type of the report
+   * @param {Array} [payload.list] new array to append to current list
+   */
+  setReportList: action((state, payload) => {
+    const { type, items } = payload;
+    switch (type) {
+      case "LOCATION":
+        state.listLocationReport.push(...items);
+        break;
+      case "POST":
+        state.listPostReport.push(...items);
+        break;
+      case "REVIEW":
+        state.listReviewReport.push(...items);
+        break;
+      case "CATCH":
+        state.listCatchReport.push(...items);
+        break;
+      default:
+    }
   }),
-  getListReportLocation: thunk(async (actions) => {
-    actions.setListReportLocation({ ...listLocationReport });
+
+  /**
+   * Reset list of of a report type
+   * @param {String} [payload.type] type of the report
+   */
+  resetReportList: action((state, payload) => {
+    switch (payload.type) {
+      case "LOCATION":
+        state.listLocationReport = [];
+        break;
+      case "POST":
+        state.listPostReport = [];
+        break;
+      case "REVIEW":
+        state.listReviewReport = [];
+        break;
+      case "CATCH":
+        state.listCatchReport = [];
+        break;
+      default:
+    }
+  }),
+
+  /**
+   * Set total page of each report list
+   * @param {String} [payload.type] type of the report
+   * @param {Number} [payload.totalPage] total page of list report
+   */
+  setTotalReportPage: action((state, payload) => {
+    const { type, totalPage } = payload;
+    switch (type) {
+      case "LOCATION":
+        state.totalLocationReportPage = totalPage;
+        break;
+      case "REVIEW":
+        state.totalReviewReportPage = totalPage;
+        break;
+      case "POST":
+        state.totalPostReportPage = totalPage;
+        break;
+      case "CATCH":
+        state.totalCatchReportPage = totalPage;
+        break;
+      default:
+    }
+  }),
+
+  /**
+   * Get location report list by page number
+   * @param {Number} [payload.pageCurrent] current page number
+   * @param {Function} [payload.setIsLoading] function to set loading state
+   */
+  getListLocationReportLocation: thunk(async (actions, payload) => {
+    const { pageNo, setIsLoading } = payload;
+    try {
+      const { data } = await http.get(API_URL.ADMIN_REPORT_LOCATION_LIST, {
+        params: { pageNo },
+      });
+      const { totalPage, items } = data;
+      actions.setTotalReportPage({ type: "LOCATION", totalPage });
+      actions.setReportList({ type: "LOCATION", items });
+      // }
+      setIsLoading(false);
+    } catch (error) {
+      // handle error
+      setIsLoading(false);
+    }
+  }),
+
+  /**
+   * Get catch report list by page number
+   * @param {Number} [payload.pageCurrent] current page number
+   * @param {Function} [payload.setIsLoading] function to set loading state
+   */
+  getListReportCatch: thunk(async (actions, payload) => {
+    const { pageNo, setIsLoading } = payload;
+    try {
+      const { data } = await http.get(API_URL.ADMIN_REPORT_CATCH_LIST, {
+        params: { pageNo },
+      });
+      const { totalPage, items } = data;
+      actions.setTotalReportPage({ type: "CATCH", totalPage });
+      actions.setReportList({ type: "CATCH", items });
+      setIsLoading(false);
+    } catch (error) {
+      // handle error
+      setIsLoading(false);
+    }
+  }),
+  /**
+   * Get post report list by page number
+   * @param {Number} [payload.pageCurrent] current page number
+   * @param {Function} [payload.setIsLoading] function to set loading state
+   */
+  getListPostReport: thunk(async (actions, payload) => {
+    const { pageNo, setIsLoading } = payload;
+    try {
+      const { data } = await http.get(API_URL.ADMIN_REPORT_POST_LIST, {
+        params: { pageNo },
+      });
+      const { totalPage, items } = data;
+      actions.setTotalReportPage({ type: "POST", totalPage });
+      actions.setReportList({ type: "POST", items });
+      setIsLoading(false);
+    } catch (error) {
+      // handle error
+      setIsLoading(false);
+    }
+  }),
+  /**
+   * Get review report list by page number
+   * @param {Number} [payload.pageCurrent] current page number
+   * @param {Function} [payload.setIsLoading] function to set loading state
+   */
+  getListReviewReport: thunk(async (actions, payload) => {
+    const { pageNo, setIsLoading } = payload;
+    try {
+      const { data } = await http.get(API_URL.ADMIN_REPORT_REVIEW_LIST, {
+        params: { pageNo },
+      });
+      const { totalPage, items } = data;
+      actions.setTotalReportPage({ type: "REVIEW", totalPage });
+      actions.setReportList({ type: "REVIEW", items });
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      // handle error
+    }
   }),
 };
 
