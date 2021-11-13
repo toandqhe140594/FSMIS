4749c01698efa65250b16bd8ff3f8793c2910bf5
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-13 15:58:17 +0700

    Style for input component

--- src/components/common/InputComponent.js
+++ src/components/common/InputComponent.js
@@ -9,6 +9,17 @@ const styles = StyleSheet.create({
   asterisk: { color: "#f43f5e", fontSize: 16 },
   bold: { fontWeight: "bold" },
   text: { fontSize: 16, marginBottom: 4 },
+  inputComponent: {
+    borderWidth: 1,
+    borderColor: "#e5e5e5",
+    borderRadius: 5,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    elevation: 5,
+  },
 });
 
 const InputComponent = ({
@@ -50,6 +61,7 @@ const InputComponent = ({
             fontSize="md"
             keyboardType={useNumPad ? "number-pad" : "default"}
             isDisabled={shouldDisable}
+            style={styles.inputComponent}
           />
         )}
       />
