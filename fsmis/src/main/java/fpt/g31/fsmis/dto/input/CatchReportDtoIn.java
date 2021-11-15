a4f601b9f24659568384cb2ebb24f1bea03b9c9f
MoeO17 <anhtdhe140447@fpt.edu.vn>
2021-11-15 17:59:17 +0700

    fix validation for all dtoIn #176

--- fsmis/src/main/java/fpt/g31/fsmis/dto/input/CatchReportDtoIn.java
+++ fsmis/src/main/java/fpt/g31/fsmis/dto/input/CatchReportDtoIn.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -19,6 +20,7 @@ public class CatchReportDtoIn {
     @NotNull
     private Long lakeId;
     @NotEmpty
+    @Valid
     private List<CatchDetailDtoIn> catchesDetailList;
     @NotNull
     private boolean hidden;
--- fsmis/src/main/java/fpt/g31/fsmis/dto/input/RegistrationDtoIn.java
+++ fsmis/src/main/java/fpt/g31/fsmis/dto/input/RegistrationDtoIn.java
@@ -1,6 +1,11 @@
 package fpt.g31.fsmis.dto.input;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -8,17 +13,24 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class RegistrationDtoIn {
 
+    @NotEmpty
     private String phone;
 
+    @NotEmpty
     private String password;
 
+    @NotEmpty
     private String fullName;
 
+    @NotNull
     private LocalDateTime dob;
 
+    @NotNull
     private Boolean gender;
 
+    @NotEmpty
     private String address;
 
+    @NotNull
     private Long wardId;
 }
