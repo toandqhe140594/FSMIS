3b5b5f71eba9991eb18affd58d86237d12d02757
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-16 15:31:19 +0700

    [Component - ListViewRoute] Add AdvanceSearchModel and connect api

--- src/components/FishingSpotSearchScreenComponents/ListViewRoute.js
+++ src/components/FishingSpotSearchScreenComponents/ListViewRoute.js
@@ -1,18 +1,30 @@
 import { useNavigation } from "@react-navigation/native";
-import { useStoreState } from "easy-peasy";
-import React from "react";
-import { FlatList, View } from "react-native";
+import { useStoreActions, useStoreState } from "easy-peasy";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 import { Button, Icon } from "react-native-elements";
 
 import styles from "../../config/styles";
 import { goToAdvanceSearchScreen } from "../../navigations";
+import { showToastMessage } from "../../utilities";
 import FLocationCard from "../FLocationCard";
 
 const ListViewRoute = () => {
   const navigation = useNavigation();
-  const advancedLocationList = useStoreState(
-    (states) => states.MapSearchModel.advancedLocationList,
+  // DucHM ADD_START 16/11/2021
+  const [isLoading, setIsLoading] = useState(false);
+  const [getStatus, setGetStatus] = useState(null);
+  const { listLocationResult } = useStoreState(
+    (states) => states.AdvanceSearchModel,
   );
+  const { getListLocationNextPage } = useStoreActions(
+    (actions) => actions.AdvanceSearchModel,
+  );
+  const handleLoadMore = () => {
+    setIsLoading(true);
+    getListLocationNextPage({ setGetStatus });
+  };
+  // DucHM ADD_END 16/11/2021
 
   // Show/hide overlay
   const goToAdvanceSearchFilterScreen = () => {
@@ -34,7 +46,31 @@ const ListViewRoute = () => {
     />
   );
 
+  // DucHM ADD_START 16/11/2021
+  const renderFooter = () => {
+    return isLoading ? <ActivityIndicator color="#2089DC" /> : null;
+  };
+
+  const renderEmpty = () => (
+    <Text style={{ alignSelf: "center", color: "gray" }}>
+      Chưa có kết quả tìm kiếm
+    </Text>
+  );
+  // DucHM ADD_END 16/11/2021
+
   const keyExtractor = (item) => item.id.toString();
+
+  // DucHM ADD_START 16/11/2021
+  useEffect(() => {
+    if (getStatus === "SUCCESS") {
+      setIsLoading(false);
+    } else if (getStatus === "FAILED") {
+      setIsLoading(false);
+      showToastMessage("Đã có lỗi xảy ra!");
+      // handle error
+    }
+  }, [getStatus]);
+  // DucHM ADD_END 16/11/2021
 
   return (
     <View style={[styles.centerBox, styles.flexBox, styles.wfull]}>
@@ -57,12 +93,18 @@ const ListViewRoute = () => {
       >
         <FlatList
           style={{ height: "100%" }}
-          data={advancedLocationList}
+          data={listLocationResult}
           renderItem={renderItem}
           keyExtractor={keyExtractor}
           ItemSeparatorComponent={ItemSeparatorComponent}
           initialNumToRender={3}
           maxToRenderPerBatch={5}
+          // DucHM ADD_START 16/11/2021
+          ListEmptyComponent={renderEmpty}
+          ListFooterComponent={renderFooter}
+          onEndReached={handleLoadMore}
+          onEndReachedThreshold={0.2}
+          // DucHM ADD_END 16/11/2021
         />
       </View>
     </View>
