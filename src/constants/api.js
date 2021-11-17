46ef9bb7f93fa80395372c263bd1a4174ac993b4
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-17 16:51:08 +0700

    feat: api url for activate and verify location

--- src/constants/api.js
+++ src/constants/api.js
@@ -16,6 +16,8 @@ export const ADMIN_REPORT_LOCATION_LIST = "admin/report/location";
 export const ADMIN_REPORT_POST_LIST = "admin/report/post";
 export const ADMIN_REPORT_REVIEW_LIST = "admin/report/review";
 export const ADMIN_FISHING_LOCATION_LIST = "admin/location";
+export const ADMIN_FISHING_LOCATION_ACTIVATE = "admin/location/active";
+export const ADMIN_FISHING_LOCATION_VERIFY = "admin/location/verify";
 
 export const STAFF_FIND_BY_PHONE = "findUserByPhone";
 export const STAFF_ADD = "staff/add";
