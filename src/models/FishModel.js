28d3b3bbb00eccb3004f6168d9782393927435f5
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-20 13:04:12 +0700

    [Model - Fish] Add updateFish and updateFishStatus thunk

--- src/models/FishModel.js
+++ src/models/FishModel.js
@@ -8,6 +8,9 @@ const model = {
   setFishList: action((state, payload) => {
     state.fishList = payload;
   }),
+  /**
+   * Get fish list from api call
+   */
   getFishList: thunk(async (actions) => {
     const { data } = await http.get(`${API_URL.ADMIN_FISH_LIST}`);
     actions.setFishList(data);
@@ -22,20 +25,40 @@ const model = {
       setDeleteSuccess(false);
     }
   }),
-  addNewFish: thunk(async (actions, payload) => {
-    // const { data } = await http.post(`${API_URL.ADMIN_FISH_ADD}`);
-    // actions.setFishList(data);
-    alert("fish added", payload);
+  /**
+   * Admin update a fish species
+   * If its id is available, perform edit
+   * Else add new
+   * @param {Object} payload.submitData updated data of the fish species
+   * @param {Function} payload.setSubmitStatus function to set update status
+   */
+  updateFish: thunk(async (actions, payload) => {
+    const { id, submitData, setSubmitStatus } = payload;
+    try {
+      if (id) {
+        await http.put(`${API_URL.ADMIN_FISH_EDIT}/${id}`, submitData);
+      } else {
+        await http.post(`${API_URL.ADMIN_FISH_ADD}`, submitData);
+      }
+      setSubmitStatus("SUCCESS");
+    } catch (error) {
+      setSubmitStatus("FAILED");
+    }
   }),
-  editFish: thunk(async (actions, payload) => {
-    // const { id, setDeleteSuccess } = payload;
-    // try {
-    //   await http.delete(`${API_URL.ADMIN_FISH_EDIT}/${id}`);
-    //   await setDeleteSuccess(true);
-    //   actions.removeFishFromList(id);
-    // } catch (error) {
-    //   setDeleteSuccess(false);
-    // }
+
+  /**
+   * Update fish species status active or inactive
+   * @param {Number} payload.id the fish's id
+   * @param {Function} payload.setSubmitStatus function to set update status
+   */
+  updateFishStatus: thunk(async (actions, payload) => {
+    const { id, setSubmitStatus } = payload;
+    try {
+      await http.patch(`${API_URL.ADMIN_FISH_UPDATE_STATUS}/${id}`);
+      setSubmitStatus("SUCCESS");
+    } catch (error) {
+      setSubmitStatus("FAILED");
+    }
   }),
   /**
    * Remove fish from the list data
