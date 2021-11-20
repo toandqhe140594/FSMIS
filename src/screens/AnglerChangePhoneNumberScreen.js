0f3eaa06f83ed026eca7dc36b9b3e7109eb36588
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-20 12:10:57 +0700

    [Angler - ChangePhoneNumber] Add hookform and custom component

--- src/screens/AnglerChangePhoneNumberScreen.js
+++ src/screens/AnglerChangePhoneNumberScreen.js
@@ -1,77 +1,84 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import {
-  Box,
-  Button,
-  Center,
-  Icon,
-  IconButton,
-  Input,
-  Text,
-  VStack,
-} from "native-base";
-import React from "react";
-import { useWindowDimensions } from "react-native";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Center, Icon, VStack } from "native-base";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Pressable, useWindowDimensions } from "react-native";
 
+import InputComponent from "../components/common/InputComponent";
 import HeaderTab from "../components/HeaderTab";
+import { ROUTE_NAMES, SCHEMA } from "../constants";
+
+const VisibilityIcon = ({ visible, toggleVisible }) => (
+  <Pressable onPress={toggleVisible}>
+    <Icon
+      alignSelf="flex-end"
+      color="muted.500"
+      as={<MaterialIcons name={visible ? "visibility-off" : "visibility"} />}
+      size={6}
+    />
+  </Pressable>
+);
+
+VisibilityIcon.propTypes = {
+  visible: PropTypes.bool,
+  toggleVisible: PropTypes.func,
+};
+
+VisibilityIcon.defaultProps = {
+  visible: false,
+  toggleVisible: () => {},
+};
 
 const ChangePhoneNumberScreen = () => {
+  const [visible, setVisible] = useState(false);
+  const methods = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(SCHEMA.CHANGE_PHONE_NUMBER_FORM),
+  });
+  const { handleSubmit } = methods;
+  const handleToggle = () => {
+    setVisible(!visible);
+  };
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <Center flex={1} minHeight={Math.round(useWindowDimensions().height)}>
       <HeaderTab name="Thay đổi số điện thoại" />
-      <VStack
-        flex={1}
-        justifyContent="center"
-        space={3}
-        mb={10}
-        w={{ base: "70%", md: "50%", lg: "30%" }}
-      >
-        {/* Phone number input field */}
-        <Text bold fontSize="md">
-          Nhập số điện thoại mới<Text color="danger.500">*</Text>
-        </Text>
-        <Input
-          InputLeftElement={
-            <Icon
-              as={<MaterialIcons name="phone-iphone" />}
-              size={5}
-              ml={2}
-              color="muted.500"
-            />
-          }
-          keyboardType="phone-pad"
-          maxLength={13}
-          paddingLeft={0}
-          placeholder="Số điện thoại"
-          size="lg"
-        />
-
-        {/* Password input field */}
-        <Text bold fontSize="md">
-          Mật khẩu<Text color="danger.500">*</Text>
-        </Text>
-        <Box position="relative" justifyContent="center">
-          <Input placeholder="Mật khẩu" size="lg" type="password" />
-          <IconButton
-            h="100%"
-            icon={
-              <Icon
-                alignSelf="flex-end"
-                as={<MaterialIcons name="visibility" />}
-                color="muted.500"
-                mr={2}
-                size={5}
-              />
-            }
-            justifyContent="center"
-            position="absolute"
-            right={0}
-            w="20%"
+      <FormProvider {...methods}>
+        <VStack
+          flex={1}
+          justifyContent="center"
+          space={3}
+          mb={10}
+          w={{ base: "70%", md: "50%", lg: "30%" }}
+        >
+          {/* Phone number input field */}
+          <InputComponent
+            label="Số điện thoại"
+            useNumPad
+            placeholder="Nhập số điện thoại"
+            controllerName="phone"
           />
-        </Box>
 
-        {/* Continue/Submit button */}
-        <Button mt={4}>Tiếp tục</Button>
-      </VStack>
+          {/* Password input field */}
+          <InputComponent
+            label="Mật khẩu"
+            placeholder="Nhập mật khẩu"
+            controllerName="password"
+            useSecureInput={visible}
+            rightIcon={
+              <VisibilityIcon visible={visible} toggleVisible={handleToggle} />
+            }
+          />
+          {/* Continue/Submit button */}
+          <Button mt={4} onPress={handleSubmit(onSubmit)}>
+            Tiếp tục
+          </Button>
+        </VStack>
+      </FormProvider>
     </Center>
   );
 };
