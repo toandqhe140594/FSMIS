e644bc3f7f8514a6f9f0cc7054dbce6b068a33bf
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-20 20:47:51 +0700

    [Admin - FishEdit] Connect api, loading and add toast msg

--- src/screens/AdminFishEditScreen.js
+++ src/screens/AdminFishEditScreen.js
@@ -10,12 +10,11 @@ import React, { useCallback, useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { Dimensions } from "react-native";
 
-// DucHM ADD_START 11/11/2021
 import InputComponent from "../components/common/InputComponent";
 import MultiImageSection from "../components/common/MultiImageSection";
 import HeaderTab from "../components/HeaderTab";
 import { ROUTE_NAMES, SCHEMA } from "../constants";
-// DucHM ADD_END 11/11/2021
+import { showToastMessage } from "../utilities";
 
 const OFFSET_BOTTOM = 85;
 // Get window height without status bar height
@@ -27,27 +26,27 @@ const AdminFishEditScreen = () => {
   const [fishId, setFishId] = useState(null);
   const [isActive, setIsActive] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
-  const [isSwitching, setIsSwitching] = useState(null);
   const [submitStatus, setSubmitStatus] = useState(null);
-  const { updateFish, updateFishStatus } = useStoreActions(
+  const { updateFish, updateFishStatus, getAdminFishList } = useStoreActions(
     (actions) => actions.FishModel,
   );
   const methods = useForm({
     mode: "onSubmit",
-    defaultValues: { fishImage: [] },
+    defaultValues: { imageArray: [] },
     resolver: yupResolver(SCHEMA.ADMIN_FISH_ADD_EDIT_FORM),
   });
   const { handleSubmit, setValue } = methods;
   const onSubmit = (data) => {
     setIsLoading(true);
-    const submitData = { ...data };
-    updateFish({ id: fishId, submitData, setSubmitStatus });
+    const image = data.imageArray[0].base64;
+    delete data.imageArray;
+    const submitData = { ...data, image };
+    updateFish({ id: fishId, submitData, setSubmitStatus, active: isActive });
   };
 
-  const handleSwitchStatus = () => {
+  const handleUpdateStatus = () => {
     setIsLoading(true);
-    setIsSwitching(true);
-    updateFishStatus({ id: fishId, setSubmitStatus });
+    updateFishStatus({ id: fishId, setSubmitStatus, active: isActive });
   };
 
   useEffect(() => {
@@ -56,40 +55,44 @@ const AdminFishEditScreen = () => {
       const { name, image, active } = route.params;
       setFishId(id);
       setValue("name", name);
-      setValue("image", [{ id: 1, base64: image }]);
+      setValue("imageArray", [{ id: 1, base64: image }]);
       setIsActive(active);
     }
   }, []);
 
   useEffect(() => {
     if (submitStatus === "SUCCESS") {
-      if (fishId) {
-        setIsLoading(false);
-        if (isSwitching) {
-          setIsSwitching(false);
-          setIsActive(!isActive);
-        }
-      } else {
+      setSubmitStatus(null);
+      if (!fishId) {
+        getAdminFishList();
+        showToastMessage("Thêm cá thành công");
         navigation.pop(1);
+      } else {
+        setIsLoading(false);
+        showToastMessage("Cập nhật cá thành công");
       }
+    } else if (submitStatus === "PATCHED") {
       setSubmitStatus(null);
-    } else if (submitStatus === "FAILED") {
       setIsLoading(false);
+      setIsActive(!isActive);
+      showToastMessage("Trạng thái của cá đã được thay đổi");
+    } else if (submitStatus === "FAILED") {
       setSubmitStatus(null);
+      setIsLoading(false);
+      showToastMessage("Đã xảy ra lỗi! Vui lòng thử lại sau");
     }
   }, [submitStatus]);
 
-  // DucHM ADD_START 11/11/2021
   useFocusEffect(
     // useCallback will listen to route.param
     useCallback(() => {
       if (route.params?.base64Array && route.params.base64Array.length) {
-        setValue("fishImage", route.params?.base64Array);
+        setValue("imageArray", route.params?.base64Array);
         navigation.setParams({ base64Array: [] });
       }
     }, [route.params]),
   );
-  // DucHM ADD_END 11/11/2021
+
   return (
     <>
       <HeaderTab name="Quản lý loại cá" />
@@ -104,11 +107,10 @@ const AdminFishEditScreen = () => {
             alignItems="center"
             space={2}
           >
-            {/* DucHM ADD_START 11/11/2021 */}
             <MultiImageSection
               containerStyle={{ width: "90%" }}
               formRoute={ROUTE_NAMES.ADMIN_FISH_MANAGEMENT_EDIT}
-              controllerName="image"
+              controllerName="imageArray"
             />
             <InputComponent
               myStyles={{ width: "90%" }}
@@ -118,8 +120,7 @@ const AdminFishEditScreen = () => {
               placeholder="Nhập tên cá"
               controllerName="name"
             />
-            {/* DucHM ADD_END 11/11/2021 */}
-            {/* DucHM ADD_START 18/11/2021 */}
+
             {fishId && (
               <Box w="90%" flexDirection="row" justifyContent="space-between">
                 <Text fontSize="md" bold>
@@ -133,7 +134,6 @@ const AdminFishEditScreen = () => {
                 </Text>
               </Box>
             )}
-            {/* DucHM ADD_END 18/11/2021 */}
           </VStack>
 
           <Button
@@ -145,21 +145,19 @@ const AdminFishEditScreen = () => {
           >
             {fishId ? "Lưu thay đổi" : "Thêm loại cá"}
           </Button>
-          {/* // DucHM ADD_START 18/11/2021 */}
           {fishId && (
             <Button
               w="80%"
               colorScheme={isActive ? "red" : "green"}
               alignSelf="center"
               marginTop={2}
-              isLoading={isLoading && isSwitching}
+              isLoading={isLoading}
               isLoadingText="Đang xử lý"
-              onPress={handleSwitchStatus}
+              onPress={handleUpdateStatus}
             >
               {isActive ? "Ẩn loại cá này" : "Bỏ ẩn loại cá này"}
             </Button>
           )}
-          {/* // DucHM ADD_END 18/11/2021 */}
         </FormProvider>
       </Box>
     </>
