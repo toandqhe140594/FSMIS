82801af9338891ee716dab10d5eb1f61819772d2
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-11-21 14:32:05 +0700

    feat: action get all address

--- src/models/UtilModel.js
+++ src/models/UtilModel.js
@@ -1,9 +1,59 @@
-import { thunk } from "easy-peasy";
+import { action, thunk } from "easy-peasy";
 
 import { API_URL } from "../constants";
 import http from "../utilities/Http";
 
 const model = {
+  addressList: [],
+  provinceList: [],
+  districtList: [],
+  wardList: [],
+  setDistrictList: action((state, payload = {}) => {
+    const { provinceId } = payload;
+    const foundIndex = state.addressList.findIndex(
+      (address) => address.id === provinceId,
+    );
+    state.districtList = state.addressList[foundIndex].districtDtoOutList;
+    state.wardList = [];
+  }),
+  setWardList: action((state, payload = {}) => {
+    const { districtId } = payload;
+    const foundIndex = state.districtList.findIndex(
+      (address) => address.id === districtId,
+    );
+    state.wardList = state.districtList[foundIndex].wardDtoOutList;
+  }),
+  setAddressList: action((state, payload) => {
+    state.addressList = payload;
+    const provinceArr = [];
+    payload.map((address) =>
+      provinceArr.push({
+        id: address.id,
+        name: address.name,
+      }),
+    );
+    state.provinceList = provinceArr;
+  }),
+  resetAddressData: action((state) => {
+    state.districtList = [];
+    state.wardList = [];
+  }),
+  getAllAddress: thunk(async (actions) => {
+    try {
+      const { data } = await http.get(`${API_URL.ADDRESS_ALL}`);
+      actions.setAddressList(data);
+    } catch (error) {
+      actions.setAddressList([]);
+    }
+  }),
+  getDistrictList: thunk(async (actions, payload) => {
+    const { provinceId } = payload;
+    actions.setDistrictList({ provinceId });
+  }),
+  getWardList: thunk(async (actions, payload) => {
+    const { districtId } = payload;
+    actions.setWardList({ districtId });
+  }),
   /**
    * Send otp to any phone number
    * @param {object} payload - params pass to function
@@ -65,6 +115,16 @@ const model = {
         phone,
         password,
       });
+      setSuccess(true);
+    } catch (error) {
+      setSuccess(false);
+    }
+  }),
+  register: thunk(async (actions, payload) => {
+    const { registerData } = payload;
+    const setSuccess = payload.setSuccess || (() => {});
+    try {
+      await http.post(`${API_URL.AUTHENTICATION_REGISTER}`, registerData);
       setSuccess(true);
     } catch (error) {
       setSuccess(false);
