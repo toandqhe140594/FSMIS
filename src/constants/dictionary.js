212e63c222724d087bac3f49178b5607450d839d
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-23 00:51:13 +0700

    [Constant/dictionary.js] Create file for constant strings

---
+++ src/constants/dictionary.js
@@ -0,0 +1,58 @@
+export const STATUS_SUCCESS = "SUCCESS";
+export const STATUS_FAILED = "FAILED";
+export const ALERT_TITLE = "Thông báo";
+export const ALERT_EDIT_PROFILE_SUCCESS_MSG =
+  "Cập nhật thông tin cá nhân thành công!";
+export const ALERT_ERROR_MSG = "Đã xảy ra lỗi! Vui lòng thử lại sau.";
+export const ANGLER_EDIT_PROFILE_HEADER_NAME = "Thông tin cá nhân";
+export const ALERT_ADD_LOCATION_SUCCESS_MSG = "Thêm điểm câu thành công";
+export const FMANAGE_ADD_LOCATION_HEADER = "Tạo điểm câu mới";
+export const ALERT_EDIT_LOCATION_SUCCESS_MSG = "Cập nhật điểm câu thành công";
+export const FMANAGE_EDIT_LOCATION_HEADER = "Thông tin điểm câu";
+
+export const FORM_FIELD_AVATAR = "avatarUrl";
+export const FORM_FIELD_FULL_NAME = "fullName";
+export const FORM_FIELD_GENDER = "gender";
+export const FORM_FIELD_ADDRESS = "address";
+export const FORM_FIELD_PROVINCE = "provinceId";
+export const FORM_FIELD_DISTRICT = "districtId";
+export const FORM_FIELD_WARD = "wardId";
+export const FORM_FIELD_IMAGE_ARRAY = "imageArray";
+export const FORM_FIELD_LOCATION_NAME = "name";
+export const FORM_FIELD_LOCATION_PHONE = "phone";
+export const FORM_FIELD_LOCATION_WEBSITE = "website";
+export const FORM_FIELD_LOCATION_DESCRIPTION = "description";
+export const FORM_FIELD_LOCATION_TIMETABLE = "timetable";
+export const FORM_FIELD_LOCATION_SERVICE = "service";
+export const FORM_FIELD_LOCATION_RULE = "rule";
+
+export const FULL_NAME_LABEL = "Họ và tên";
+export const GENDER_LABEL = "Giới tính";
+export const ADDRESS_LABEL = "Địa chỉ";
+export const PROVINCE_LABEL = "Tỉnh/Thành phố";
+export const DISTRICT_LABEL = "Quận/Huyện";
+export const WARD_LABEL = "Phường/xã";
+export const CONFIRM_BUTTON_LABEL = "Xác nhận";
+export const LOCATION_NAME_LABEL = "Tên điểm câu";
+export const LOCATION_PHONE_LABEL = "Số điện thoại";
+export const LOCATION_WEBSITE_LABEL = "Website";
+export const LOCATION_DESCRIPTION_LABEL = "Mô tả khu hồ";
+export const LOCATION_TIMETABLE_LABEL = "Thời gian hoạt động";
+export const LOCATION_SERVICE_LABEL = "Dịch vụ";
+export const LOCATION_RULE_LABEL = "Nội quy";
+
+export const INPUT_NAME_PLACEHOLDER = "Nhập họ và tên";
+export const SELECT_BIRTHDATE_PLACEHOLDER = "Chọn ngày sinh";
+export const SELECT_GENDER_PLACEHOLDER = "Chọn giới tính";
+export const INPUT_ADDRESS_PLACEHOLDER = "Nhập địa chỉ thường trú";
+export const SELECT_PROVINCE_PLACEHOLDER = "Chọn tỉnh/thành phố";
+export const SELECT_DISTRICT_PLACEHOLDER = "Chọn quận/huyện";
+export const SELECT_WARD_PLACEHOLDER = "Chọn phường/xã";
+export const INPUT_LOCATION_NAME_PLACEHOLDER = "Nhập tên địa điểm câu";
+export const INPUT_LOCATION_PHONE_PLACEHOLDER = "Nhập số điện thoại";
+export const INPUT_LOCATION_WEBSITE_PLACEHOLDER = "Nhập website/facebook";
+export const INPUT_LOCATION_DESCRIPTION_PLACEHOLDER = "Miêu tả khu hồ của bạn";
+export const INPUT_LOCATION_TIMETABLE_PLACEHOLDER =
+  "Miêu tả thời gian hoạt động của khu hồ";
+export const INPUT_LOCATION_SERVICE_PLACEHOLDER = "Miêu tả dịch vụ khu hồ";
+export const INPUT_LOCATION_RULE_PLACEHOLDER = "Miêu tả nội quy khu hồ";
