a74aa26a86c2a83340d229d9f1cf24b6b000b053
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-23 21:43:36 +0700

    [Component/CheckboxSelector] Update checkbox selector

---
+++ src/components/common/CheckboxSelectorComponent.js
@@ -0,0 +1,107 @@
+import { Checkbox, Select } from "native-base";
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { Controller, useFormContext, useWatch } from "react-hook-form";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  error: { color: "#f43f5e", fontSize: 12, fontStyle: "italic" },
+  title: { fontSize: 16, marginBottom: 4, fontWeight: "bold" },
+  text: { fontSize: 16, marginBottom: 4 },
+});
+
+const defaultValue = "0";
+
+const CheckboxSelectorComponent = ({
+  label,
+  isTitle,
+  placeholder,
+  data,
+  controllerName,
+  myStyles,
+}) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const [userSelection, setUserSelection] = useState(placeholder);
+  const watchSelection = useWatch({
+    control,
+    name: controllerName,
+  });
+  useEffect(() => {
+    if (watchSelection.length === 0) setUserSelection(placeholder);
+    else setUserSelection(`${watchSelection.length} lựa chọn`);
+  }, [watchSelection]);
+  return (
+    <View style={myStyles}>
+      {label.length > 0 && (
+        <Text style={isTitle ? styles.title : styles.text}>{label}</Text>
+      )}
+      <Controller
+        control={control}
+        name={controllerName}
+        render={({ field: { onChange, value } }) => (
+          <Select defaultValue={defaultValue} fontSize="md">
+            <Select.Item
+              isDisabled
+              label={userSelection}
+              value={defaultValue}
+            />
+            <Checkbox.Group value={value} onChange={onChange}>
+              {data.map((item) => (
+                <Select.Item
+                  key={item.id}
+                  isDisabled
+                  label={
+                    <Checkbox size="md" value={item.id}>
+                      {item.name}
+                    </Checkbox>
+                  }
+                />
+              ))}
+            </Checkbox.Group>
+          </Select>
+        )}
+      />
+      {errors[controllerName]?.message && (
+        <Text style={styles.error}>{errors[controllerName]?.message}</Text>
+      )}
+    </View>
+  );
+};
+
+// <CheckboxComponent
+//  defaultValue={value}
+//  onValueChange={handleValueChange(onChange)}
+//  data={data}
+/// >;
+
+// const handleValueChange = useCallback(
+//   (handler) => (value) => {
+//     handler(value);
+//   },
+//   [],
+// );
+
+CheckboxSelectorComponent.propTypes = {
+  label: PropTypes.string,
+  isTitle: PropTypes.bool,
+  placeholder: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.object),
+  controllerName: PropTypes.string,
+  myStyles: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  ),
+};
+
+CheckboxSelectorComponent.defaultProps = {
+  label: "",
+  isTitle: false,
+  data: [],
+  placeholder: "",
+  controllerName: "",
+  myStyles: {},
+};
+
+export default CheckboxSelectorComponent;
