e8cce9af07a765d6415a2fbabc426f6b27f501d9
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-24 23:36:27 +0700

    [Component/InputWithClipboard]

---
+++ src/components/common/InputWithClipboard.jsx
@@ -0,0 +1,44 @@
+import { FontAwesome5 } from "@expo/vector-icons";
+import * as Clipboard from "expo-clipboard";
+import PropTypes from "prop-types";
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import { Pressable } from "react-native";
+
+import InputComponent from "./InputComponent";
+
+const ClipboardIcon = ({ handlePress }) => (
+  <Pressable style={{ marginHorizontal: 12 }} onPress={handlePress}>
+    <FontAwesome5 name="clipboard" size={24} color="black" />
+  </Pressable>
+);
+
+const InputWithClipboard = ({ label, placeholder, controllerName }) => {
+  const { setValue } = useFormContext();
+  const pasteData = async () => {
+    const data = await Clipboard.getStringAsync();
+    if (data) setValue(controllerName, data);
+  };
+  return (
+    <InputComponent
+      label={label}
+      placeholder={placeholder}
+      controllerName={controllerName}
+      rightIcon={<ClipboardIcon handlePress={pasteData} />}
+    />
+  );
+};
+
+ClipboardIcon.propTypes = { handlePress: PropTypes.func.isRequired };
+InputWithClipboard.propTypes = {
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  controllerName: PropTypes.string,
+};
+InputWithClipboard.defaultProps = {
+  label: "",
+  placeholder: "",
+  controllerName: "",
+};
+
+export default InputWithClipboard;
