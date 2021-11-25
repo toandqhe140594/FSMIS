5e3833132a452f7b706994def2dac5471fad0c01
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-25 19:36:58 +0700

    [Angler/AuthenticationContainer] Mount AddressModel

--- src/utilities/AuthenticationContainer.js
+++ src/utilities/AuthenticationContainer.js
@@ -3,18 +3,25 @@ import React, { useEffect } from "react";
 import { ToastAndroid } from "react-native";
 
 import { ROLE_USER } from "../constants";
+import AddressModel from "../models/AddressModel";
 import AdminStackNavigator from "../navigations/AdminStackNavigator";
 import AuthenticationStackNavigator from "../navigations/AuthenticationStackNavigator";
 import RootStackNavigator from "../navigations/RootStackNavigator";
 import LogoScreen from "../screens/LogoScreen";
+import store from "./Store";
+
+store.addModel("AddressModel", AddressModel);
 
 const AuthenticationContainer = () => {
   const loginState = useStoreState((states) => states.loginState);
   const userRole = useStoreState((states) => states.userRole);
   const errorMessage = useStoreState((states) => states.errorMessage);
   const retrieveToken = useStoreActions((actions) => actions.retrieveToken);
-
+  const getAllProvince = useStoreActions(
+    (actions) => actions.AddressModel.getAllProvince,
+  );
   useEffect(() => {
+    getAllProvince();
     setTimeout(async () => {
       await retrieveToken();
     }, 1500);
