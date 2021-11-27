a62534964174b8d0b67b6beb9c63b9cf157d991b
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-28 01:55:54 +0700

    [FManageSelectScreen] Remove comment

--- src/screens/FManageSelectScreen.js
+++ src/screens/FManageSelectScreen.js
@@ -1,14 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { useStoreActions, useStoreState } from "easy-peasy";
 import React, { useEffect, useMemo, useState } from "react";
-import {
-  ActivityIndicator,
-  FlatList,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 
 import OverlayLoading from "../components/common/OverlayLoading";
 import FLocationCard from "../components/FLocationCard";
@@ -40,11 +33,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignSelf: "center",
   },
-  loadingContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
 });
 
 const FManageSelectScreen = () => {
@@ -67,7 +55,6 @@ const FManageSelectScreen = () => {
   };
 
   useEffect(() => {
-    // setLoading(true);
     getListOfFishingLocations({ setGetSuccess });
     const timeout = setTimeout(() => {
       setLoading(false);
