4b95f248fd3dd3ae0495ec3d1bf4889ca24fd982
TuesdayTaco71 <hoangminhduc29018@gmail.com>
2021-11-30 23:02:57 +0700

    [FManage/CheckinHistory] Add loading

--- src/screens/FManageCheckinHistory.js
+++ src/screens/FManageCheckinHistory.js
@@ -1,12 +1,20 @@
 import { useStoreActions, useStoreState } from "easy-peasy";
-import { Box, Divider, FlatList, Text } from "native-base";
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Box, FlatList, Text } from "native-base";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 import AvatarCard from "../components/AvatarCard";
 import CheckInCard from "../components/CheckInCard";
 import DateRangeModalSelector from "../components/common/DateRangeModalSelector";
+import SmallScreenLoadingIndicator from "../components/common/SmallScreenLoadingIndicator";
 import HeaderTab from "../components/HeaderTab";
 import PressableCustomCard from "../components/PressableCustomCard";
+import { DICTIONARY } from "../constants";
 
 const renderItem = ({ item }) => (
   <Box
@@ -30,6 +38,8 @@ const FManageCheckinHistoryScreen = () => {
   const { checkinHistoryCurrentPage, checkinHistoryList } = useStoreState(
     (states) => states.FManageModel,
   );
+  const needRefresh = useRef(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const {
@@ -46,6 +56,17 @@ const FManageCheckinHistoryScreen = () => {
     [checkinHistoryList && checkinHistoryList.length > 0],
   );
 
+  const stopLoading = () => {
+    if (needRefresh.current) needRefresh.current = false;
+    setIsLoading(false);
+  };
+
+  const loadAllList = () => {
+    setIsLoading(true);
+    needRefresh.current = false;
+    getCheckinHistoryList().finally(stopLoading);
+  };
+
   const dateChangeHandler = useCallback(
     (date, type) => {
       if (type === "END_DATE") {
@@ -59,35 +80,46 @@ const FManageCheckinHistoryScreen = () => {
   );
 
   const onLoadMore = () => {
+    setIsLoading(true);
     const objParams = {};
     if (startDate !== null) {
       if (endDate !== null) {
         objParams.endDate = endDate;
       }
       objParams.startDate = startDate;
-      getCheckinHistoryListByDate(objParams);
+      getCheckinHistoryListByDate(objParams).finally(stopLoading);
     } else {
-      getCheckinHistoryList();
+      getCheckinHistoryList().finally(stopLoading);
     }
   };
 
   const submitDateFilterHandler = useCallback(() => {
+    needRefresh.current = true;
     resetCheckinHistory();
     onLoadMore();
   }, [startDate, endDate]);
 
-  const renderEmpty = () => (
-    <Text color="gray.400" alignSelf="center">
-      Lịch sử check-in đang trống
-    </Text>
-  );
+  const renderHeader = () =>
+    isLoading && needRefresh.current ? <SmallScreenLoadingIndicator /> : null;
+
+  const renderEmpty = () =>
+    !isLoading && (
+      <Text color="gray.400" alignSelf="center">
+        Lịch sử check-in đang trống
+      </Text>
+    );
+
+  const renderFooter = () =>
+    isLoading && !needRefresh.current ? (
+      <SmallScreenLoadingIndicator containerStyle={{ marginVertical: 12 }} />
+    ) : null;
 
   const keyExtractor = (item, index) => index.toString();
 
   useEffect(() => {
     // If the current page = 1 aka the list is empty then call api to init the list
     if (checkinHistoryCurrentPage === 1) {
-      getCheckinHistoryList();
+      getCheckinHistoryList().finally(stopLoading);
     }
     return () => {
       resetCheckinHistory(); // Clear list data when screen unmount
@@ -96,24 +128,25 @@ const FManageCheckinHistoryScreen = () => {
 
   return (
     <Box>
-      <HeaderTab name="Lịch sử Check-in" />
+      <HeaderTab name={DICTIONARY.FMANAGE_CHECK_IN_HISTORY_HEADER} />
       <Box w={{ base: "100%", md: "25%" }}>
         <DateRangeModalSelector
+          doExtraOnSelectValue={loadAllList}
           handleDatePickerChange={dateChangeHandler}
           onSubmitDate={submitDateFilterHandler}
         />
         <FlatList
-          h="82%"
+          h="87%"
           data={checkinHistoryList}
           renderItem={renderItem}
           keyExtractor={keyExtractor}
           onEndReached={onLoadMore}
+          ListHeaderComponent={renderHeader}
           ListEmptyComponent={renderEmpty}
+          ListFooterComponent={renderFooter}
           contentContainerStyle={memoizedStyle}
+          onEndReachedThreshold={0.1}
         />
-        <Box mt="10">
-          <Divider />
-        </Box>
       </Box>
     </Box>
   );
