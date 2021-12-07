7b250085104018cb14845747f93a03e257782984
Dao Quoc Toan <toandqhe140594@fpt.edu.vn>
2021-12-07 15:48:50 +0700

    chore: remove redundant if

--- src/models/FManageModel.js
+++ src/models/FManageModel.js
@@ -616,13 +616,9 @@ const model = {
     const { currentId } = getState();
     const { setDeleteSuccess } = payload;
     try {
-      const { status } = await http.delete(
-        `${API_URL.LOCATION_CLOSE}/${currentId}`,
-      );
-      if (status === 200) {
-        actions.getListOfFishingLocations();
-        setDeleteSuccess(true);
-      }
+      await http.delete(`${API_URL.LOCATION_CLOSE}/${currentId}`);
+      actions.getListOfFishingLocations();
+      setDeleteSuccess(true);
     } catch (error) {
       setDeleteSuccess(false);
     }
